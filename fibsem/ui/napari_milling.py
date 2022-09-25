79ac54ed31561825dd37081092a7db8d391360e6
Patrick Cleeve <patrick.cleeve@monash.edu>
2022-09-25 23:12:38 +1000

    start napari milling

---
+++ fibsem/ui/napari_milling.py
@@ -0,0 +1,65 @@
+import logging
+import sys
+from enum import Enum
+
+import numpy as np
+import scipy.ndimage as ndi
+from autoscript_sdb_microscope_client import SdbMicroscopeClient
+from autoscript_sdb_microscope_client.structures import (MoveSettings,
+                                                         StagePosition)
+from fibsem import acquire, conversions, movement, constants, alignment
+from fibsem.structures import BeamType, MicroscopeSettings
+from fibsem.ui import utils as fibsem_ui
+from fibsem.ui.qtdesigner_files import NapariMilling
+from PyQt5 import QtCore, QtWidgets
+
+import napari
+
+# TODO: maybe have to change to dialog?
+
+
+class NapariMillingUI(NapariMilling.Ui_MainWindow, QtWidgets.QMainWindow):
+    def __init__(
+        self, viewer: napari.Viewer, parent = None
+    ):
+        super(NapariMillingUI, self).__init__(parent = parent)
+        self.setupUi(self)
+
+        self.setup_connections()
+
+        self.viewer = viewer
+    
+    def setup_connections(self):
+        print("setup connections")
+
+
+        self.pushButton_run_milling.clicked.connect(self.run_milling)
+
+
+    def run_milling(self):
+
+        print("run milling")
+
+        print(f"Shapes: {self.viewer.layers['Shapes'].shape_type}")
+        print(f"Data: {self.viewer.layers['Shapes'].data}")
+
+
+# TODO: override enter
+
+
+def main():
+    # from liftout import utils
+    # from fibsem.ui import windows as fibsem_ui_windows
+    # microscope, settings= utils.quick_setup()
+
+
+    app = QtWidgets.QApplication([])
+    viewer = napari.Viewer(ndisplay=2)
+    napari_milling_ui = NapariMillingUI(viewer=viewer)
+    viewer.window.add_dock_widget(napari_milling_ui, area='right')  
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
---
+++ fibsem/ui/qtdesigner_files/NapariMilling.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'NapariMilling.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(430, 916)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.frame = QtWidgets.QFrame(self.centralwidget)
+        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setObjectName("frame")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.frame)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.label_3 = QtWidgets.QLabel(self.frame)
+        self.label_3.setObjectName("label_3")
+        self.gridLayout_2.addWidget(self.label_3, 2, 0, 1, 1)
+        self.label_4 = QtWidgets.QLabel(self.frame)
+        self.label_4.setObjectName("label_4")
+        self.gridLayout_2.addWidget(self.label_4, 3, 0, 1, 1)
+        self.label_5 = QtWidgets.QLabel(self.frame)
+        self.label_5.setObjectName("label_5")
+        self.gridLayout_2.addWidget(self.label_5, 4, 0, 1, 1)
+        self.label = QtWidgets.QLabel(self.frame)
+        self.label.setObjectName("label")
+        self.gridLayout_2.addWidget(self.label, 0, 0, 1, 3)
+        self.label_2 = QtWidgets.QLabel(self.frame)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout_2.addWidget(self.label_2, 1, 0, 1, 3)
+        self.pushButton_run_milling = QtWidgets.QPushButton(self.frame)
+        self.pushButton_run_milling.setObjectName("pushButton_run_milling")
+        self.gridLayout_2.addWidget(self.pushButton_run_milling, 5, 0, 1, 3)
+        self.gridLayout.addWidget(self.frame, 0, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 430, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label_3.setText(_translate("MainWindow", "TextLabel"))
+        self.label_4.setText(_translate("MainWindow", "TextLabel"))
+        self.label_5.setText(_translate("MainWindow", "TextLabel"))
+        self.label.setText(_translate("MainWindow", "Napari Milling"))
+        self.label_2.setText(_translate("MainWindow", "TextLabel"))
+        self.pushButton_run_milling.setText(_translate("MainWindow", "Run Milling"))
+
