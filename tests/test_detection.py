149d75cb1b9fbcbfdf5a4fab01bf6d5d1412e33c
Patrick Cleeve <patrick.cleeve@monash.edu>
2022-09-26 18:58:05 +1000

    [FEAT] detect corners + tests

---
+++ tests/test_detection.py
@@ -0,0 +1,43 @@
+import pytest
+
+
+import numpy as np
+from fibsem.structures import Point
+from fibsem.detection import detection
+
+
+
+
+def test_detect_corner():
+
+    # set square pattern
+    arr = np.ones(shape=(100, 100))
+    arr = np.pad(arr, pad_width=100, mode = "constant", constant_values=0)
+
+    # detect corners
+    top_right = detection.detect_corner(arr)
+    top_left = detection.detect_corner(arr, left=True)
+    bottom_right = detection.detect_corner(arr, left=False, bottom=True)
+    bottom_left = detection.detect_corner(arr, left=True, bottom=True)
+
+    assert top_right == Point(199, 100)
+    assert top_left == Point(100, 100)
+    assert bottom_right == Point(199, 199)
+    assert bottom_left == Point(100, 199)
+
+
+def test_detect_closest_edge():
+
+    # set square pattern
+    arr = np.ones(shape=(100, 100))
+    arr = np.pad(arr, pad_width=100, mode = "constant", constant_values=0)
+
+    # detect corners 
+    pt = Point(0, 0)
+    top = detection.detect_closest_edge_v2(arr, pt)
+
+    pt = Point(299, 299)
+    bot = detection.detect_closest_edge_v2(arr, pt)
+
+    assert top == Point(100, 100)
+    assert bot == Point(199, 199)
