4e10c4fbbf79e0a5ac4b03b1c60009aa81ca26ba
Lachlan Burne <Lachie.Burne@monash.edu>
2022-10-10 11:46:45 +1100

    added way to convert to correct directory format

--- fibsem/segmentation/utils.py
+++ fibsem/segmentation/utils.py
@@ -48,5 +48,22 @@ def convert_to_tiff(path_ini, img_ext, lab_ext, path_save=None):
         label = Image.open(label)
         label.save(os.path.join(path, "label.tiff"))  # or 'test.tif'
 
+def convert_to_folders(img_dir, label_dir, save_path, img_extension, label_extension):
+    images_sorted = sorted(glob.glob(os.path.join(img_dir, f"*.{img_extension}")))
+    masks_sorted = sorted(glob.glob(os.path.join(label_dir, f"*.{label_extension}")))
 
-convert_img_size("G:\\DeMarco\\train", [1536, 1024])
+    for x, (im, label) in enumerate(zip(images_sorted, masks_sorted)):
+        num_folder = str(x).zfill(9) 
+        path = os.path.join(save_path, num_folder)  
+        
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+        im = Image.open(im)
+        im.save(os.path.join(path, "image.tiff"))  # or 'test.tif'
+
+        label = Image.open(label)
+        label.save(os.path.join(path, "label.tiff"))  # or 'test.tif'
+
+# convert_img_size("G:\\DeMarco\\train", [1536, 1024])
+convert_to_folders()
