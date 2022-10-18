8e30061345467edf0248ad037333c3c808fa276d
Lachlan Burne <Lachie.Burne@monash.edu>
2022-10-18 11:16:27 +1100

    Added some functionality to FibsemLabellingUI.py

--- fibsem/ui/FibsemLabellingUI.py
+++ fibsem/ui/FibsemLabellingUI.py
@@ -6,7 +6,10 @@ import fibsem
 import napari
 import napari.utils.notifications
 import yaml
+import glob
+import numpy as np
 from autoscript_sdb_microscope_client import SdbMicroscopeClient
+from PIL import Image
 from fibsem import calibration, constants, utils
 from fibsem.structures import (
     BeamSystemSettings,
@@ -30,6 +33,7 @@ class FibsemLabellingUI(FibsemLabellingUI.Ui_Dialog, QtWidgets.QDialog):
         super(FibsemLabellingUI, self).__init__(parent=parent)
         self.setupUi(self)
         self.viewer = viewer
+        self.idx = 0
 
 
         self.setup_connections()
@@ -47,28 +51,39 @@ class FibsemLabellingUI(FibsemLabellingUI.Ui_Dialog, QtWidgets.QDialog):
         print("load_data")
 
         # read raw data
-        raw_path = self.lineEdit_raw_data.text()
-        save_path = self.lineEdit_save_path.text()
+        self.raw_path = self.lineEdit_raw_data.text()
+        self.save_path = self.lineEdit_save_path.text()
 
-        print(raw_path)
-        print(save_path)
+        print(self.raw_path)
+        print(self.save_path)
 
         # create required directories
+        self.filenames = sorted(glob.glob(os.path.join(self.raw_path, "*.tif*")))
 
         # initialise viewer layers
-
+        img = Image.open(self.filenames[0])
+        self.viewer.layers.clear()
+        self.viewer.add_image(img, name="img")
+        self.viewer.add_labels(np.zeros_like(img, dtype=np.uint8), name="Labels")
 
     def next_image(self):
 
         print("next image")
 
         # save current image
+        fname = str(self.idx).zfill(9)
+        
+        self.viewer.layers["img"].save(os.path.join(self.save_path, "images", f"{fname}.tif"))
+        label = self.viewer.layers["Labels"].data.astype(np.uint8)
+
+        im = Image.fromarray(label) 
+        im.save(os.path.join(self.save_path, "labels", f"{fname}.tif"))  # or 'test.tif'
 
         # advance index
+        self.idx += 1
 
         # update / clear viewer
-        import numpy as np
-        img = np.random.random(size=(500, 1000))
+        img = Image.open(self.filenames[self.idx])
         self.viewer.layers.clear()
         self.viewer.add_image(img, name="img")
         self.viewer.add_labels(np.zeros_like(img, dtype=np.uint8), name="Labels")
@@ -79,11 +94,22 @@ class FibsemLabellingUI(FibsemLabellingUI.Ui_Dialog, QtWidgets.QDialog):
         print("previous_image")
 
         # save current image
+        fname = str(self.idx).zfill(9)
+        
+        self.viewer.layers["img"].save(os.path.join(self.save_path, "images", f"{fname}.tif"))
+        label = self.viewer.layers["Labels"].data.astype(np.uint8)
+
+        im = Image.fromarray(label) 
+        im.save(os.path.join(self.save_path, "labels", f"{fname}.tif"))  # or 'test.tif'
 
         # decrement index
+        self.idx -= 1
 
         # update / clear viewer
-
+        img = Image.open(self.filenames[self.idx])
+        self.viewer.layers.clear()
+        self.viewer.add_image(img, name="img")
+        self.viewer.add_labels(np.zeros_like(img, dtype=np.uint8), name="Labels")
 
 
 
