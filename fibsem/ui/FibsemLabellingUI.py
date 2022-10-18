ff60bc5fde1757e62df354fc7121ec9e3e52f260
Patrick Cleeve <patrick.cleeve@monash.edu>
2022-10-18 19:17:56 +1100

    updated ui functionality

--- fibsem/ui/FibsemLabellingUI.py
+++ fibsem/ui/FibsemLabellingUI.py
@@ -9,16 +9,6 @@ import numpy as np
 import tifffile as tff
 import zarr
 from PIL import Image
-""" import yaml
-from autoscript_sdb_microscope_client import SdbMicroscopeClient
-from fibsem import calibration, constants, utils
-from fibsem.structures import (
-    BeamSystemSettings,
-    BeamType,
-    MicroscopeSettings,
-    StageSettings,
-    SystemSettings,
-) """
 from qtdesigner_files import FibsemLabellingUI
 from PyQt5 import QtWidgets
 
@@ -60,41 +50,40 @@ class FibsemLabellingUI(FibsemLabellingUI.Ui_Dialog, QtWidgets.QDialog):
         vol = tff.imread(os.path.join(raw_path, "*.tif*"), aszarr=True) # loading folder of .tif into zarr array)
         self.zarr_set = zarr.open(vol)
         self.filenames = sorted(glob.glob(os.path.join(raw_path, "*.tif*")))
+        
         # create required directories        
         os.makedirs(os.path.join(self.save_path, "images"), exist_ok=True)
         os.makedirs(os.path.join(self.save_path, "labels"), exist_ok=True)
-        # initialise viewer layers
-        self.img = self.zarr_set[self.idx]
-        self.fname = self.filenames[self.idx]
-        self.viewer.add_image(self.img, name="img")
-        self.viewer.add_labels(np.zeros_like(self.img), name="Labels")
+        
+        self.update_image()
+
 
     def next_image(self):
 
         print("next image")
 
-        # save current image
-        bname = os.path.basename(self.fname).split(".")[0]
-        
-        self.viewer.layers["img"].save(os.path.join(self.save_path, "images", f"{bname}.tif"))
-        label = self.viewer.layers["Labels"].data.astype(np.uint8)
+        self.save_image()
 
-        im = Image.fromarray(label) 
-        im.save(os.path.join(self.save_path, "labels", f"{bname}.tif"))  # or 'test.tif'
         # advance index
-        self.idx = self.idx+1
-        # update / clear viewer
-        self.viewer.layers.clear()
-        self.img = self.zarr_set[self.idx]
-        self.fname = self.filenames[self.idx]
-        self.viewer.add_image(self.img, name="img")
-        self.viewer.add_labels(np.zeros_like(self.img), name="Labels")
+        self.idx +=1
+        self.idx = np.clip(self.idx, 0, len(self.filenames)-1)
 
+        self.update_image()
 
     def previous_image(self):
 
         print("previous_image")
 
+        self.save_image()
+
+        # decrement index
+        self.idx -= 1
+        self.idx = np.clip(self.idx, 0, len(self.filenames)-1)
+        
+        self.update_image()
+
+    def save_image(self):
+
         # save current image
         bname = os.path.basename(self.fname).split(".")[0]
         
@@ -103,20 +92,39 @@ class FibsemLabellingUI(FibsemLabellingUI.Ui_Dialog, QtWidgets.QDialog):
 
         im = Image.fromarray(label) 
         im.save(os.path.join(self.save_path, "labels", f"{bname}.tif"))  # or 'test.tif'
-        # advance index
-        self.idx = self.idx-1
+        
+    def update_image(self):
+        
+        # update progress text
+        self.label_progress.setText(f"{self.idx + 1}/{len(self.filenames)}")        
+
         # update / clear viewer
         self.viewer.layers.clear()
         self.img = self.zarr_set[self.idx]
         self.fname = self.filenames[self.idx]
         self.viewer.add_image(self.img, name="img")
-        self.viewer.add_labels(np.zeros_like(self.img), name="Labels")
 
+        label_image = self.get_label_image()
+        self.viewer.add_labels(label_image, name="Labels")
 
+        # disable buttons
+        IS_NOT_FIRST_INDEX = bool(self.idx != 0)
+        IS_NOT_LAST_INDEX = bool(self.idx != int(len(self.filenames)-1))
+        self.pushButton_previous.setEnabled(IS_NOT_FIRST_INDEX)
+        self.pushButton_next.setEnabled(IS_NOT_LAST_INDEX)
 
+    def get_label_image(self) -> np.ndarray:
 
+        if os.path.basename(self.fname).split(".")[0] in os.listdir(os.path.join(self.save_path, "images")): 
+            label_image = tff.imread(os.path.join(self.save_path, "labels", self.fname))
+        else:
+            label_image = np.zeros_like(self.img)
 
-    #TODO: port functionality
+        return label_image
+
+    # TODO: port functionality
+    # TODO: show existing labels if exist
+    # TODO: remove use of PIl, use tf to save
 
 def main():
 
@@ -126,7 +134,6 @@ def main():
         fibsem_labelling_ui, area="right", add_vertical_stretch=False
     )
     napari.run()
-
 
 if __name__ == "__main__":
     main()
