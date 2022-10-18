dc1a81878a041a5dc54c2f1ea96a2d015c4affe8
LucileNaegele <lucile.naegele@monash.edu>
2022-10-18 11:29:36 +1100

    Update FibsemLabellingUI.py

--- fibsem/ui/FibsemLabellingUI.py
+++ fibsem/ui/FibsemLabellingUI.py
@@ -1,15 +1,16 @@
 import logging
 import os
 import sys
-
+import glob
 import fibsem
 import napari
 import napari.utils.notifications
-import yaml
-import glob
 import numpy as np
-from autoscript_sdb_microscope_client import SdbMicroscopeClient
+import tifffile as tff
+import zarr
 from PIL import Image
+""" import yaml
+from autoscript_sdb_microscope_client import SdbMicroscopeClient
 from fibsem import calibration, constants, utils
 from fibsem.structures import (
     BeamSystemSettings,
@@ -17,8 +18,8 @@ from fibsem.structures import (
     MicroscopeSettings,
     StageSettings,
     SystemSettings,
-)
-from fibsem.ui.qtdesigner_files import FibsemLabellingUI
+) """
+from qtdesigner_files import FibsemLabellingUI
 from PyQt5 import QtWidgets
 
 BASE_PATH = os.path.join(os.path.dirname(fibsem.__file__), "config")
@@ -35,7 +36,6 @@ class FibsemLabellingUI(FibsemLabellingUI.Ui_Dialog, QtWidgets.QDialog):
         self.viewer = viewer
         self.idx = 0
 
-
         self.setup_connections()
 
     def setup_connections(self):
@@ -51,42 +51,44 @@ class FibsemLabellingUI(FibsemLabellingUI.Ui_Dialog, QtWidgets.QDialog):
         print("load_data")
 
         # read raw data
-        self.raw_path = self.lineEdit_raw_data.text()
+        raw_path = self.lineEdit_raw_data.text()
         self.save_path = self.lineEdit_save_path.text()
 
-        print(self.raw_path)
+        print(raw_path)
         print(self.save_path)
 
-        # create required directories
-        self.filenames = sorted(glob.glob(os.path.join(self.raw_path, "*.tif*")))
-
+        vol = tff.imread(os.path.join(raw_path, "*.tif*"), aszarr=True) # loading folder of .tif into zarr array)
+        self.zarr_set = zarr.open(vol)
+        self.filenames = sorted(glob.glob(os.path.join(raw_path, "*.tif*")))
+        # create required directories        
+        os.makedirs(os.path.join(self.save_path, "images"), exist_ok=True)
+        os.makedirs(os.path.join(self.save_path, "labels"), exist_ok=True)
         # initialise viewer layers
-        img = Image.open(self.filenames[0])
-        self.viewer.layers.clear()
-        self.viewer.add_image(img, name="img")
-        self.viewer.add_labels(np.zeros_like(img, dtype=np.uint8), name="Labels")
+        self.img = self.zarr_set[self.idx]
+        self.fname = self.filenames[self.idx]
+        self.viewer.add_image(self.img, name="img")
+        self.viewer.add_labels(np.zeros_like(self.img), name="Labels")
 
     def next_image(self):
 
         print("next image")
 
         # save current image
-        fname = str(self.idx).zfill(9)
+        bname = os.path.basename(self.fname).split(".")[0]
         
-        self.viewer.layers["img"].save(os.path.join(self.save_path, "images", f"{fname}.tif"))
+        self.viewer.layers["img"].save(os.path.join(self.save_path, "images", f"{bname}.tif"))
         label = self.viewer.layers["Labels"].data.astype(np.uint8)
 
         im = Image.fromarray(label) 
-        im.save(os.path.join(self.save_path, "labels", f"{fname}.tif"))  # or 'test.tif'
-
+        im.save(os.path.join(self.save_path, "labels", f"{bname}.tif"))  # or 'test.tif'
         # advance index
-        self.idx += 1
-
+        self.idx = self.idx+1
         # update / clear viewer
-        img = Image.open(self.filenames[self.idx])
         self.viewer.layers.clear()
-        self.viewer.add_image(img, name="img")
-        self.viewer.add_labels(np.zeros_like(img, dtype=np.uint8), name="Labels")
+        self.img = self.zarr_set[self.idx]
+        self.fname = self.filenames[self.idx]
+        self.viewer.add_image(self.img, name="img")
+        self.viewer.add_labels(np.zeros_like(self.img), name="Labels")
 
 
     def previous_image(self):
@@ -94,22 +96,22 @@ class FibsemLabellingUI(FibsemLabellingUI.Ui_Dialog, QtWidgets.QDialog):
         print("previous_image")
 
         # save current image
-        fname = str(self.idx).zfill(9)
+        bname = os.path.basename(self.fname).split(".")[0]
         
-        self.viewer.layers["img"].save(os.path.join(self.save_path, "images", f"{fname}.tif"))
+        self.viewer.layers["img"].save(os.path.join(self.save_path, "images", f"{bname}.tif"))
         label = self.viewer.layers["Labels"].data.astype(np.uint8)
 
         im = Image.fromarray(label) 
-        im.save(os.path.join(self.save_path, "labels", f"{fname}.tif"))  # or 'test.tif'
-
-        # decrement index
-        self.idx -= 1
-
+        im.save(os.path.join(self.save_path, "labels", f"{bname}.tif"))  # or 'test.tif'
+        # advance index
+        self.idx = self.idx-1
         # update / clear viewer
-        img = Image.open(self.filenames[self.idx])
         self.viewer.layers.clear()
-        self.viewer.add_image(img, name="img")
-        self.viewer.add_labels(np.zeros_like(img, dtype=np.uint8), name="Labels")
+        self.img = self.zarr_set[self.idx]
+        self.fname = self.filenames[self.idx]
+        self.viewer.add_image(self.img, name="img")
+        self.viewer.add_labels(np.zeros_like(self.img), name="Labels")
+
 
 
 
