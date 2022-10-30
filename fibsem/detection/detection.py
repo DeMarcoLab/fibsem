ffeb1f76c8482244bf95c6f3aa61925f8766e46d
Patrick Cleeve <patrick.cleeve@monash.edu>
2022-10-31 10:47:58 +1100

    det

--- fibsem/detection/detection.py
+++ fibsem/detection/detection.py
@@ -465,3 +465,87 @@ def detect_bounding_box(mask, color, threshold=25):
 
     return bbox
 
+
+
+
+### v2
+from fibsem.segmentation.model import SegmentationModel
+from fibsem import conversions
+from dataclasses import dataclass
+
+@dataclass
+class DetectedFeatures:
+    features: list[Feature]
+    image: np.ndarray
+    mask: np.ndarray
+    pixelsize: float
+    distance: Point
+
+def detect_features_v2(img: np.ndarray, mask: np.ndarray, features: tuple[Feature]) -> list[Feature]:
+
+    detection_features = []
+
+    for feature in features:
+        
+        det_type = feature.detection_type
+        initial_point = feature.feature_px
+        
+        if not isinstance(det_type, FeatureType):
+            raise TypeError(f"Detection Type {det_type} is not supported.")
+
+        # get the initial position estimate
+        if initial_point is None:
+            initial_point = Point(x=img.shape[1]//2, y=img.shape[0]//2)
+
+        if det_type == FeatureType.ImageCentre:
+            feature_px = initial_point
+
+        if det_type == FeatureType.NeedleTip:
+            feature_px = detect_needle_v4(mask)
+
+        if det_type in [FeatureType.LamellaCentre, FeatureType.LamellaLeftEdge, FeatureType.LamellaRightEdge]:
+            feature_px = detect_lamella(mask, det_type)
+
+        if det_type == FeatureType.LandingPost:
+            feature_px = detect_landing_post_v3(img, initial_point)
+
+        detection_features.append(
+            Feature(detection_type=det_type, feature_px=feature_px)
+        )
+
+    return detection_features
+
+def locate_shift_between_features_v2(image: np.ndarray, model: SegmentationModel, features: tuple[Feature], pixelsize: float) -> DetectedFeatures:
+
+    # model inference
+    mask = model.inference(image)
+
+    # detect features 
+    feature_1, feature_2 = detect_features_v2(image, mask, features)
+
+    # calculate distance between features
+    distance_px = conversions.distance_between_points(feature_1.feature_px, feature_2.feature_px)
+    distance_m = conversions.convert_point_from_pixel_to_metres(distance_px, pixelsize)
+
+    det = DetectedFeatures(
+        features=[feature_1, feature_2],
+        image = image,
+        mask = mask,
+        distance = distance_m,
+        pixelsize = pixelsize
+    )
+
+    return det
+
+def plot_det_result_v2(det: DetectedFeatures):
+
+    fig, ax = plt.subplots(1, 2, figsize=(12, 7))
+    ax[0].imshow(det.image, cmap="gray")
+    ax[0].set_title(f"Image")
+    ax[1].imshow(det.mask)
+    ax[1].set_title("Prediction")
+    ax[1].plot(det.features[0].feature_px.x, det.features[0].feature_px.y, "g+", ms=20, label=det.features[0].detection_type.name)
+    ax[1].plot(det.features[1].feature_px.x, det.features[1].feature_px.y, "w+", ms=20, label=det.features[1].detection_type.name)
+    ax[1].plot([det.features[0].feature_px.x, det.features[1].feature_px.x], [det.features[0].feature_px.y, det.features[1].feature_px.y], "w--")
+    ax[1].legend(loc="best")
+    plt.show()
