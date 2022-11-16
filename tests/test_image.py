180f737ba08daa61a20cfa0aec9e139dfa0f2c4f
Lachlan Burne <Lachie.Burne@monash.edu>
2022-11-16 14:42:21 +1100

    fibsemImage update

--- tests/test_image.py
+++ tests/test_image.py
@@ -14,14 +14,6 @@ def test_image():
     assert img.data.shape[1] == 32
     
 def test_metadata():
-     array1 = np.random.rand(32,32)
-    img = fb.fibsemImage(array1)
-    img.save_to_TIFF('test.tif')
-    img.load_from_TIFF('test.tif')
-    assert np.array_equal(array1, img.data)
-    assert img.data.shape[0] == 32
-    assert img.data.shape[1] == 32
-    
     pass
 
 
