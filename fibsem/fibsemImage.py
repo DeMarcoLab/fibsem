9389f554938b562519611a71ae26d048ec6d04ac
LucileNaegele <lucile.naegele@monash.edu>
2022-11-17 14:07:49 +1100

    syntax improvements

--- fibsem/fibsemImage.py
+++ fibsem/fibsemImage.py
@@ -7,20 +7,18 @@ import json
 from fibsem.structures import BeamType, GammaSettings, ImageSettings
 from autoscript_sdb_microscope_client.structures import AdornedImage
 
+
 @dataclass
-class Metadata:
+class FibsemMetadata:
     image_settings: ImageSettings
 
     def __to_dict__(self):
         settings_dict = self.image_settings.__to_dict__()
         return settings_dict
 
-class fibsemImage():
-    def __init__(
-        self,
-        data: np.ndarray = None,
-        metadata: Metadata = None
-    ):  
+
+class FibsemImage:
+    def __init__(self, data: np.ndarray = None, metadata: FibsemMetadata = None):
         if data is not None:
             self.check_data(data)
             self.data = data
@@ -29,40 +27,37 @@ class fibsemImage():
         else:
             self.metadata = None
 
-    def load_from_TIFF(self, tiff_path):
-        tiff_image = tff.TiffFile(tiff_path)
-        self.data = tiff_image.asarray()
-        try:
-            metadata = json.loads(tiff_image.pages[0].tags["ImageDescription"].value) 
-            self.metadata = Metadata(
-                image_settings=ImageSettings.__from_dict__(metadata)                
-            )
-        except:
-            self.metadata = None
+    def load(self, tiff_path):
+        with tff.TiffFile(tiff_path) as tiff_image:
+            self.data = tiff_image.asarray()
+            try:
+                metadata = json.loads(tiff_image.pages[0].tags["ImageDescription"].value)
+                self.metadata = FibsemMetadata(
+                    image_settings=ImageSettings.__from_dict__(metadata)
+                )
+            except:
+                self.metadata = None
 
-    def save_to_TIFF(self, save_path: str):
+    def save(self, save_path: str):
         if self.metadata is not None:
-                metadata_dict = self.metadata.__to_dict__()
-                tff.imwrite(
-                    save_path, 
-                    self.data,
-                    metadata=metadata_dict,
-                )
+            metadata_dict = self.metadata.__to_dict__()
         else:
-                tff.imwrite(
-                    save_path,
-                    self.data,
-                    metadata=None,
-                )
-        
-    def convert_adorned_to_fibsemImage(self, adorned: AdornedImage, metadata: ImageSettings = None):
+            metadata_dict = None
+        tff.imwrite(
+            save_path,
+            self.data,
+            metadata=metadata_dict,
+        )
+
+    def convert_adorned_to_fibsemImage(
+        self, adorned: AdornedImage, metadata: ImageSettings = None
+    ):
         self.data = adorned.data
         self.check_data(self.data)
-        self.metadata  = metadata
+        self.metadata = metadata
 
     def check_data(self, data):
         assert data.ndim == 2 or data.ndim == 3
         assert data.dtype == np.uint8
         if data.ndim == 3 and data.shape[2] == 1:
-            data = data[:,:,0]
-            
+            data = data[:, :, 0]
--- tests/test_image.py
+++ tests/test_image.py
@@ -8,56 +8,59 @@ from autoscript_sdb_microscope_client.structures import AdornedImage
 
 
 def test_image():
-    array1 = np.uint8(255*np.random.rand(32,32))
-    img = fb.fibsemImage(array1)
-    img.save_to_TIFF('test.tif')
-    img.load_from_TIFF('test.tif')
+    array1 = np.uint8(255 * np.random.rand(32, 32))
+    img = fb.FibsemImage(array1)
+    img.save("test.tif")
+    img.load("test.tif")
     assert np.array_equal(array1, img.data)
     assert img.data.shape[0] == 32
     assert img.data.shape[1] == 32
     assert img.data.all() >= 0
     assert img.data.all() <= 255
     assert img.data.dtype == np.uint8
-    
+
+
 def test_metadata():
-    array1 = np.uint8(np.zeros((256,128)))
+    array1 = np.uint8(np.zeros((256, 128)))
     gamma_dict = {
         "enabled": True,
         "min_gamma": 0.5,
         "max_gamma": 1.8,
         "scale_factor": 0.01,
-        "threshold": 46
+        "threshold": 46,
     }
     Image_settings = {
         "resolution": "32x32",
-        "dwell_time": 1.e-6,
-        "hfw": 150.e-6,
+        "dwell_time": 1.0e-6,
+        "hfw": 150.0e-6,
         "autocontrast": True,
-        "beam_type": "ELECTRON", 
+        "beam_type": "ELECTRON",
         "gamma": gamma_dict,
         "save": False,
         "save_path": "path",
-        "label": "label"
+        "label": "label",
     }
 
-    metadata = fb.Metadata(
-            image_settings = ImageSettings.__from_dict__(Image_settings)
-        )
-    img = fb.fibsemImage(array1, metadata)
-    img.save_to_TIFF('test.tif')
-    img.load_from_TIFF('test.tif')
+    metadata = fb.FibsemMetadata(
+        image_settings=ImageSettings.__from_dict__(Image_settings)
+    )
+    img = fb.FibsemImage(array1, metadata)
+    img.save("test.tif")
+    img.load("test.tif")
     assert np.array_equal(array1, img.data)
     assert img.data.shape[0] == 256
     assert img.data.shape[1] == 128
     assert img.metadata == metadata
-    
+
+
 def test_adorned_image_conversion():
-    array1 = np.uint8(255*np.random.rand(32,32))
-    img1 = fb.fibsemImage(array1)
+    array1 = np.uint8(255 * np.random.rand(32, 32))
+    img1 = fb.FibsemImage(array1)
     adorned = AdornedImage(array1)
-    img2 = fb.fibsemImage()
+    img2 = fb.FibsemImage()
     img2.convert_adorned_to_fibsemImage(adorned)
     assert np.array_equal(img1.data, img2.data)
+
 
 def test_adorned_metadata_conversion():
     gamma_dict = {
@@ -65,29 +68,25 @@ def test_adorned_metadata_conversion():
         "min_gamma": 0.5,
         "max_gamma": 1.8,
         "scale_factor": 0.01,
-        "threshold": 46
+        "threshold": 46,
     }
     Image_settings = {
         "resolution": "32x32",
-        "dwell_time": 1.e-6,
-        "hfw": 150.e-6,
+        "dwell_time": 1.0e-6,
+        "hfw": 150.0e-6,
         "autocontrast": True,
-        "beam_type": "ELECTRON", 
+        "beam_type": "ELECTRON",
         "gamma": gamma_dict,
         "save": False,
         "save_path": "path",
-        "label": "label"
+        "label": "label",
     }
     image_settings = ImageSettings.__from_dict__(Image_settings)
-    metadata = fb.Metadata(
-            image_settings
-        )
+    metadata = fb.FibsemMetadata(image_settings)
 
-    array1 = np.uint8(255*np.random.rand(32,32))
-    img1 = fb.fibsemImage(array1, metadata=metadata)
+    array1 = np.uint8(255 * np.random.rand(32, 32))
+    img1 = fb.FibsemImage(array1, metadata=metadata)
     adorned = AdornedImage(array1)
-    img2 = fb.fibsemImage()
+    img2 = fb.FibsemImage()
     img2.convert_adorned_to_fibsemImage(adorned, metadata=metadata)
     assert img1.metadata == img2.metadata
-
-
