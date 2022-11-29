724d345ceec0f03bec77647938d3d8ecd0554351
LucileNaegele <lucile.naegele@monash.edu>
2022-11-30 09:39:22 +1100

    Update FibsemMicroscope.py

--- fibsem/FibsemMicroscope.py
+++ fibsem/FibsemMicroscope.py
@@ -10,4 +10,50 @@ class FibsemMicroscope(ABC):
     def disconnect(self):
         pass
 
+    @abstractmethod
+    def get_microscope_state(self):
+        pass
     
+    @abstractmethod
+    def patterning(self):
+        pass
+
+    @abstractmethod
+    def get_needle_position(self):
+        pass
+
+    @abstractmethod
+    def move_needle(self, position):
+        pass
+
+    @abstractmethod
+    def insert_needle(self):
+        pass
+
+    @abstractmethod
+    def retract_needle(self):
+        pass    
+
+    @abstractmethod
+    def get_stage_position(self):
+        pass
+
+    @abstractmethod
+    def move_stage(self, position):
+        pass        
+
+    @abstractmethod
+    def get_eb_settings(self):
+        pass
+
+    @abstractmethod
+    def set_eb_settings(self, settings):
+        pass
+
+    @abstractmethod
+    def get_ib_settings(self):
+        pass
+
+    @abstractmethod
+    def set_ib_settings(self, settings):
+        pass    
