6dc4a48db2eccdc39d78a86085b6db016569d53e
LucileNaegele <lucile.naegele@monash.edu>
2022-11-30 11:14:31 +1100

    converted to class method

--- fibsem/FibsemMicroscope.py
+++ fibsem/FibsemMicroscope.py
@@ -16,22 +16,22 @@ class FibsemMicroscope(ABC):
 
 
 class ThermoMicroscope(FibsemMicroscope):
-    def __init__(self, session_path: Path = None, config_path: Path = None, protocol_path: Path = None, setup_logging: bool = True):
-        self.setup_session()
+    def __init__(self):
+        self.microscope = SdbMicroscopeClient()
+        self.settings = None
 
-
-    @staticmethod
+    @classmethod
     def setup_session(
-        self,
+        cls,
         session_path: Path = None,
         config_path: Path = None,
         protocol_path: Path = None,
         setup_logging: bool = True,
     ):
-        self.settings = utils.load_settings_from_config(config_path, protocol_path)
+        settings = utils.load_settings_from_config(config_path, protocol_path)
 
         # create session directories
-        session = f'{self.settings.protocol["name"]}_{utils.current_timestamp()}'
+        session = f'{settings.protocol["name"]}_{utils.current_timestamp()}'
         if protocol_path is None:
             protocol_path = os.getcwd()
 
@@ -45,24 +45,28 @@ class ThermoMicroscope(FibsemMicroscope):
             utils.configure_logging(session_path)
 
         # connect to microscope
-        self.__connect_to_microscope__(ip_address=self.settings.system.ip_address)
+        microscope = ThermoMicroscope.__connect_to_microscope__(ip_address=settings.system.ip_address)
 
         # image_setttings
-        self.settings.image.save_path = session_path
+        settings.image.save_path = session_path
 
-        logging.info(f"Finished setup for session: {session}")        
+        logging.info(f"Finished setup for session: {session}")   
+
+        return cls(microscope, settings)
+
 
     def disconnect(self):
         pass
-
+    
     def __connect_to_microscope__(self, ip_address: str, port: int):
         """Connect to the FIBSEM microscope."""
         try:
             # TODO: get the port
             logging.info(f"Microscope client connecting to [{ip_address}:{port}]")
-            self.microscope = SdbMicroscopeClient()
-            self.microscope.connect(host=ip_address, port=port)
+            microscope = SdbMicroscopeClient()
+            microscope.connect(host=ip_address, port=port)
             logging.info(f"Microscope client connected to [{ip_address}:{port}]")
         except Exception as e:
             logging.error(f"Unable to connect to the microscope: {e}")
-            self.microscope = None
+            microscope = None
+        return microscope
