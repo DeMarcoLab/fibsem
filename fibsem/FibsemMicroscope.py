c3322219638fee83ff5b4b3308b4789192b66c41
LucileNaegele <lucile.naegele@monash.edu>
2022-11-30 11:09:25 +1100

    microscope set up

--- fibsem/FibsemMicroscope.py
+++ fibsem/FibsemMicroscope.py
@@ -1,59 +1,68 @@
 from abc import ABC, abstractmethod
+import fibsem.utils as utils
+from pathlib import Path
+import os
+import logging
+from autoscript_sdb_microscope_client import SdbMicroscopeClient
 
 class FibsemMicroscope(ABC):
-
     @abstractmethod
-    def connect(self, host: str, port: int):
+    def setup_session(self, host: str, port: int):
         pass
 
     @abstractmethod
     def disconnect(self):
         pass
 
-    @abstractmethod
-    def get_microscope_state(self):
-        pass
-    
-    @abstractmethod
-    def patterning(self, patterning_mode: str, patterning_settings: dict):
-        pass
 
-    @abstractmethod
-    def get_needle_position(self):
-        pass
+class ThermoMicroscope(FibsemMicroscope):
+    def __init__(self, session_path: Path = None, config_path: Path = None, protocol_path: Path = None, setup_logging: bool = True):
+        self.setup_session()
 
-    @abstractmethod
-    def move_needle(self, position):
-        pass
 
-    @abstractmethod
-    def insert_needle(self):
-        pass
+    @staticmethod
+    def setup_session(
+        self,
+        session_path: Path = None,
+        config_path: Path = None,
+        protocol_path: Path = None,
+        setup_logging: bool = True,
+    ):
+        self.settings = utils.load_settings_from_config(config_path, protocol_path)
 
-    @abstractmethod
-    def retract_needle(self):
-        pass    
+        # create session directories
+        session = f'{self.settings.protocol["name"]}_{utils.current_timestamp()}'
+        if protocol_path is None:
+            protocol_path = os.getcwd()
 
-    @abstractmethod
-    def get_stage_position(self):
-        pass
+        # configure paths
+        if session_path is None:
+            session_path = os.path.join(os.path.dirname(protocol_path), session)
+        os.makedirs(session_path, exist_ok=True)
 
-    @abstractmethod
-    def move_stage(self, position):
-        pass        
+        # configure logging
+        if setup_logging:
+            utils.configure_logging(session_path)
 
-    @abstractmethod
-    def get_eb_settings(self):
-        pass
+        # connect to microscope
+        self.__connect_to_microscope__(ip_address=self.settings.system.ip_address)
 
-    @abstractmethod
-    def set_eb_settings(self, settings):
-        pass
+        # image_setttings
+        self.settings.image.save_path = session_path
 
-    @abstractmethod
-    def get_ib_settings(self):
+        logging.info(f"Finished setup for session: {session}")        
+
+    def disconnect(self):
         pass
 
-    @abstractmethod
-    def set_ib_settings(self, settings):
-        pass    
+    def __connect_to_microscope__(self, ip_address: str, port: int):
+        """Connect to the FIBSEM microscope."""
+        try:
+            # TODO: get the port
+            logging.info(f"Microscope client connecting to [{ip_address}:{port}]")
+            self.microscope = SdbMicroscopeClient()
+            self.microscope.connect(host=ip_address, port=port)
+            logging.info(f"Microscope client connected to [{ip_address}:{port}]")
+        except Exception as e:
+            logging.error(f"Unable to connect to the microscope: {e}")
+            self.microscope = None
