94d94e3f6cbd266e5b27cc932745260e7a5fada4
LucileNaegele <lucile.naegele@monash.edu>
2022-12-21 11:38:17 +1100

    GUI Update

--- fibsem/ui/connect_ui_check.py
+++ fibsem/ui/connect_ui_check.py
@@ -13,11 +13,20 @@ class MainWindow(QtWidgets.QMainWindow, connect.Ui_MainWindow):
     def __init__(self,*args,obj=None,**kwargs) -> None:
         super(MainWindow,self).__init__(*args,**kwargs)
         self.setupUi(self)
+
+        # Buttons setup
+
         self.ConnectButton.clicked.connect(self.connect_to_microscope)
         self.DisconnectButton.clicked.connect(self.disconnect_from_microscope)
         self.RefImage.clicked.connect(self.take_reference_images)
         self.ResetImage.clicked.connect(self.reset_images)
+        self.EB_Click.clicked.connect(self.click_EB_Image)
+        self.IB_click.clicked.connect(self.click_IB_Image)
+
+        # Initialise microscope object
         self.microscope = None
+
+        # Gamma and Image Settings
 
         self.gamma_settings = GammaSettings(
             enabled=True,
@@ -74,7 +83,7 @@ class MainWindow(QtWidgets.QMainWindow, connect.Ui_MainWindow):
     def take_reference_images(self):
         
         if self.microscope is None:
-            self.update_log('No Microscope Connected')
+            self.update_log("No Microscope Connected")
             return
 
         # take image with both beams
@@ -87,10 +96,34 @@ class MainWindow(QtWidgets.QMainWindow, connect.Ui_MainWindow):
         # viewer.add_image(eb_image.data, name="EB Image")
         # viewer.add_image(ib_image.data, name="IB Image")
 
+    def click_EB_Image(self):
+
+        if self.microscope is None:
+            self.update_log("No Microscope Connected")
+            return
+
+        tmp_beam_type = self.image_settings.beam_type
+        self.image_settings.beam_type = BeamType.ELECTRON
+        eb_image = acquire.new_image(self.microscope, self.image_settings)
+        self.EB_Image = ui_utils.set_arr_as_qlabel(eb_image.data, self.EB_Image, shape=(400, 400))
+        self.image_settings.beam_type = tmp_beam_type
+    
+    def click_IB_Image(self):
+
+        if self.microscope is None:
+            self.update_log("No Microscope Connected")
+            return
+
+        tmp_beam_type = self.image_settings.beam_type
+        self.image_settings.beam_type = BeamType.ION
+        ib_image = acquire.new_image(self.microscope, self.image_settings)
+        self.IB_Image = ui_utils.set_arr_as_qlabel_8(ib_image.data, self.IB_Image, shape=(400, 400))
+        self.image_settings.beam_type = tmp_beam_type
+
     def reset_images(self):
 
-        self.EB_Image.setText("No Image to Display")
-        self.IB_Image.setText("No Image to Display")
+        self.EB_Image.setText("")
+        self.IB_Image.setText("")
         
     
 
