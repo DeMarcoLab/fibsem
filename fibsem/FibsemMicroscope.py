4d6fdd6fdbd8827aebf868bd6a5d989f36698f63
LucileNaegele <lucile.naegele@monash.edu>
2023-01-12 11:10:54 +1100

    circular import fixes

--- fibsem/FibsemMicroscope.py
+++ fibsem/FibsemMicroscope.py
@@ -318,8 +318,8 @@ class ThermoMicroscope(FibsemMicroscope):
         wd = self.connection.beams.electron_beam.working_distance.value
 
         # calculate stage movement
-        x_move = move.x_corrected_stage_movement(dx)
-        yz_move = move.y_corrected_stage_movement(
+        x_move = x_corrected_stage_movement(dx)
+        yz_move = y_corrected_stage_movement(
             microscope=self.connection,
             settings=settings,
             expected_y=dy,
@@ -630,8 +630,8 @@ class TescanMicroscope(FibsemMicroscope):
         wd = self.connection.SEM.Optics.GetWD()
 
         # calculate stage movement
-        x_move = move.x_corrected_stage_movement(dx)
-        yz_move = move.y_corrected_stage_movement(
+        x_move = x_corrected_stage_movement(dx)
+        yz_move = y_corrected_stage_movement(
             microscope=self.connection,
             settings=settings,
             expected_y=dy,
@@ -650,3 +650,132 @@ class TescanMicroscope(FibsemMicroscope):
         # self.connection.specimen.stage.link() # TODO how to link for TESCAN?
 
         return
+
+def rotation_angle_is_larger(angle1: float, angle2: float, atol: float = 90) -> bool:
+    """Check the rotation angles are large
+
+    Args:
+        angle1 (float): angle1 (radians)
+        angle2 (float): angle2 (radians)
+        atol : tolerance (degrees)
+
+    Returns:
+        bool: rotation angle is larger than atol
+    """
+
+    return angle_difference(angle1, angle2) > (np.deg2rad(atol))
+
+
+def rotation_angle_is_smaller(angle1: float, angle2: float, atol: float = 5) -> bool:
+    """Check the rotation angles are large
+
+    Args:
+        angle1 (float): angle1 (radians)
+        angle2 (float): angle2 (radians)
+        atol : tolerance (degrees)
+
+    Returns:
+        bool: rotation angle is smaller than atol
+    """
+
+    return angle_difference(angle1, angle2) < (np.deg2rad(atol))
+
+
+def angle_difference(angle1: float, angle2: float) -> float:
+    """Return the difference between two angles, accounting for greater than 360, less than 0 angles
+
+    Args:
+        angle1 (float): angle1 (radians)
+        angle2 (float): angle2 (radians)
+
+    Returns:
+        float: _description_
+    """
+    angle1 %= 2 * np.pi
+    angle2 %= 2 * np.pi
+
+    large_angle = np.max([angle1, angle2])
+    small_angle = np.min([angle1, angle2])
+
+    return min((large_angle - small_angle), ((2 * np.pi + small_angle - large_angle)))
+
+def x_corrected_stage_movement(
+    expected_x: float,
+) -> FibsemStagePosition:
+    """Calculate the x corrected stage movement.
+
+    Args:
+        expected_x (float): distance along x-axis
+
+    Returns:
+        StagePosition: x corrected stage movement (relative position)
+    """
+    return FibsemStagePosition(x=expected_x, y=0, z=0)
+
+
+def y_corrected_stage_movement(
+    microscope: FibsemMicroscope,
+    settings: MicroscopeSettings,
+    expected_y: float,
+    beam_type: BeamType = BeamType.ELECTRON,
+) -> FibsemStagePosition:
+    """Calculate the y corrected stage movement, corrected for the additional tilt of the sample holder (pre-tilt angle).
+
+    Args:
+        microscope (SdbMicroscopeClient, optional): autoscript microscope instance
+        settings (MicroscopeSettings): microscope settings
+        expected_y (float, optional): distance along y-axis.
+        beam_type (BeamType, optional): beam_type to move in. Defaults to BeamType.ELECTRON.
+
+    Returns:
+        StagePosition: y corrected stage movement (relative position)
+    """
+
+    # TODO: replace with camera matrix * inverse kinematics
+    # TODO: replace stage_tilt_flat_to_electron with pre-tilt
+
+    # all angles in radians
+    stage_tilt_flat_to_electron = np.deg2rad(
+        settings.system.stage.tilt_flat_to_electron
+    )
+    stage_tilt_flat_to_ion = np.deg2rad(settings.system.stage.tilt_flat_to_ion)
+
+    stage_rotation_flat_to_eb = np.deg2rad(
+        settings.system.stage.rotation_flat_to_electron
+    ) % (2 * np.pi)
+    stage_rotation_flat_to_ion = np.deg2rad(
+        settings.system.stage.rotation_flat_to_ion
+    ) % (2 * np.pi)
+
+    # current stage position
+    current_stage_position = microscope.get_stage_position()
+    stage_rotation = current_stage_position.r % (2 * np.pi)
+    stage_tilt = current_stage_position.t
+
+    PRETILT_SIGN = 1.0
+    # pretilt angle depends on rotation
+    if rotation_angle_is_smaller(stage_rotation, stage_rotation_flat_to_eb, atol=5):
+        PRETILT_SIGN = 1.0
+    if rotation_angle_is_smaller(stage_rotation, stage_rotation_flat_to_ion, atol=5):
+        PRETILT_SIGN = -1.0
+
+    corrected_pretilt_angle = PRETILT_SIGN * stage_tilt_flat_to_electron
+
+    # perspective tilt adjustment (difference between perspective view and sample coordinate system)
+    if beam_type == BeamType.ELECTRON:
+        perspective_tilt_adjustment = -corrected_pretilt_angle
+        SCALE_FACTOR = 1.0  # 0.78342  # patented technology
+    elif beam_type == BeamType.ION:
+        perspective_tilt_adjustment = -corrected_pretilt_angle - stage_tilt_flat_to_ion
+        SCALE_FACTOR = 1.0
+
+    # the amount the sample has to move in the y-axis
+    y_sample_move = (expected_y * SCALE_FACTOR) / np.cos(
+        stage_tilt + perspective_tilt_adjustment
+    )
+
+    # the amount the stage has to move in each axis
+    y_move = y_sample_move * np.cos(corrected_pretilt_angle)
+    z_move = y_sample_move * np.sin(corrected_pretilt_angle)
+
+    return FibsemStagePosition(x=0, y=y_move, z=z_move)
