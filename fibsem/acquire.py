b459314457c93c3ab6753c3d59c3cbcb0f3393cc
Sergey Gorelick <gorliks@gmail.com>
2023-02-17 12:16:29 +1100

    added CLAHE filter for image enhancement

--- fibsem/acquire.py
+++ fibsem/acquire.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 
 from skimage import exposure
+import cv2
 
 from fibsem.structures import (
     BeamType,
@@ -138,6 +139,77 @@ def auto_gamma(
     image_data = exposure.adjust_gamma(image.data, gam)
 
     return FibsemImage(data=image_data, metadata=image.metadata)
+
+
+
+
+def apply_clahe(
+    image: FibsemImage,
+    which_package: str = "skimage",
+    clip_limit_cv2: float = 15,
+    tile_grid_size: int = 8,
+    clip_limit_skimage: float = 0.02,
+    kernel_size = None
+) -> FibsemImage:
+    """
+    Applies Contrast Limited Adaptive Histogram Equalisation correction to the input `FibsemImage`.
+    image is divided into small blocks called "tiles" (tileSize is 8x8 by default in OpenCV). Then each of these
+    blocks are histogram equalized as usual. So in a small area, histogram would confine to a small region
+    (unless there is noise). If noise is there, it will be amplified. To avoid this, contrast limiting is applied.
+    If any histogram bin is above the specified contrast limit (by default 40 in OpenCV), those pixels are clipped and
+    distributed uniformly to other bins before applying histogram equalization. After equalization, to remove artifacts
+    in tile borders, bilinear interpolation is applied.
+
+    In OpenCV tileGridSize (tile_grid_size) is by default 8x8, clipLimit (clip_limit_cv2) is by default 40
+
+    In skimage kernel_size (int or array_like), is optional. It defines the shape of contextual regions used in the
+    algorithm. By default, kernel_size is 1/8 of image height by 1/8 of its width.
+    clip_limit float, optional. Clipping limit, normalized between 0 and 1 (higher values give more contrast).
+
+    Args:
+        image (FibsemImage): The input `FibsemImage` to apply gamma correction to.
+
+        type (str) Either "skimage" or "OpenCV" to apply the filter from the corresponding library
+
+        clip_limit_cv2 (float): used if which_package=="OpenCV". Defaults to 15. (by default 40 in OpenCV)
+        tile_grid_size (int): used if which_package=='OpenCV'. Defaults to 8x8 pixels.
+
+        clip_limit_skimage (float): used if which_package=="skimage". Defaults to 0.01. Clipping limit, normalised between 0 and 1 (higher values give more contrast).
+        tile_grid_size (int): used if which_package=='skimage'. if None, defaults to kernel_size is 1/8 of image height by 1/8 of its width.
+
+    Returns:
+        A new `FibsemImage` object containing the clahe-enhanced image data, with the same metadata
+        as the input image.
+
+    Notes:
+        - This function applies gamma correction to the input image using either the `cv2.createCLAHE` or `skimage.exposure.equalize_adapthist` function
+        - The `FibsemImage` object in the returned list contains the clahe-enhanced image data as a
+          numpy array, as well as other image metadata.
+    """
+
+    """
+        OpenCV requires 8-bit images for CLAHE, skimage requires either 8-bit images or arrays with values between [0,1]
+        Here, we convert the raw data into an 8-bit image to proceed
+    """
+
+    temp = image.data
+    temp = temp / temp.max()
+    temp = (temp * 2**8).astype(np.uint8)
+
+    if which_package=='OpenCV':
+        tile_grid_size = int(tile_grid_size)
+        clahe = cv2.createCLAHE(clipLimit=clip_limit_cv2,
+                                tileGridSize=(tile_grid_size,tile_grid_size))
+        image_data = clahe.apply(temp)
+
+    else: # default filter
+        # nbin = 256 default, for 8-bit images
+        image_data = exposure.equalize_adapthist(temp,
+                                                 kernel_size=kernel_size,
+                                                 clip_limit=clip_limit_skimage, nbins=256)
+
+    return FibsemImage(data=image_data, metadata=image.metadata)
+
 
 
 def new_image(
