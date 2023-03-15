ec1e663d6fbbd9437a091894ede3082f8d0fb365
Rohit-Kannachel <rohit.kannachel@monash.edu>
2023-03-15 11:58:30 +1100

    Basic pytest setup in New_test (tescan)

--- new_tests/test_connection.py
+++ new_tests/test_connection.py
@@ -7,9 +7,12 @@ def microscope_connection():
 
     tescan_microscope,settings = utils.setup_session(ip_address='localhost',manufacturer='Tescan')
 
-    return tescan_microscope,settings
+    return [tescan_microscope,settings]
 
-def connection(tescan_microscope,settings):
+def test_connection(microscope_connection):
+
+    tescan_microscope = microscope_connection[0]
+    settings = microscope_connection[1]
 
     assert tescan_microscope is not None
     assert isinstance(tescan_microscope,microscope.TescanMicroscope)
