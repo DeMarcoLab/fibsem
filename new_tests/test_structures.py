3f4424fa1bf2cf2b4a11e4576a5a8cb3c00a837d
Rohit-Kannachel <rohit.kannachel@monash.edu>
2023-03-16 16:03:22 +1100

    testing

---
+++ new_tests/test_structures.py
@@ -0,0 +1,36 @@
+import pytest
+from fibsem import structures
+
+def test_point():
+
+    p1 = structures.Point(2,3)
+
+    p_dict = p1.__to_dict__()
+
+    assert p_dict["x"] == 2
+    assert p_dict["y"] == 3
+
+    p_made_dict = {"x":5,"y":6}
+
+    p2 = structures.Point.__from_dict__(p_made_dict)
+
+    assert p2.x == 5
+    assert p2.y == 6
+
+    p_list = p2.__to_list__()
+
+    assert p_list[0] == 5
+    assert p_list[1] == 6
+
+    p_made_list = [-2,-9]
+
+    p3 = structures.Point.__from_list__(p_made_list)
+
+    assert p3.x == -2
+    assert p3.y == -9
+
+    p1.x = 5
+
+    assert p1.x == 5
+
+
