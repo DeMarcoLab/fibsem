59a0d617fc0b00483234edd5890bca7df423e562
Rohit-Kannachel <rohit.kannachel@monash.edu>
2023-04-13 13:59:46 +1000

    GIS widget stuff

--- fibsem/ui/FibsemGISWidget.py
+++ fibsem/ui/FibsemGISWidget.py
@@ -28,16 +28,32 @@ class FibsemGISWidget(FibsemGISWidget.Ui_Form, QtWidgets.QWidget):
         self.settings = settings
         self.viewer = viewer
         self.image_widget = image_widget
+        self.GIS_inserted = False
+        self.GIS_insert_status_label.setText(f"GIS Status: inserted" if self.GIS_inserted else "GIS Status: retracted")
+
 
         self.setup_connections()
 
         self.update_ui()
 
     def setup_connections(self):
-        pass
+        self.insertGIS_button.clicked.connect(self.insert_retract_gis)
+        
     
     def update_ui(self):
         pass
+
+    def insert_retract_gis(self):
+        if self.GIS_inserted:
+            # self.microscope.retract_gis()
+            self.GIS_inserted = False
+            self.insertGIS_button.setText("Insert GIS")
+            self.GIS_insert_status_label.setText("GIS Status: retracted")
+        else:
+            # self.microscope.insert_gis()
+            self.GIS_inserted = True
+            self.insertGIS_button.setText("Retract GIS")
+            self.GIS_insert_status_label.setText("GIS Status: inserted")
 
 def main():
 
