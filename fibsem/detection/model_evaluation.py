d532cc9b5d1fe624840d0a8265b776eb77abc523
Rohit-Kannachel <rohit.kannachel@monash.edu>
2023-04-18 12:40:55 +1000

    metrics saved and images captured

--- fibsem/detection/model_evaluation.py
+++ fibsem/detection/model_evaluation.py
@@ -18,15 +18,21 @@ from dataclasses import dataclass
 from fibsem import conversions
 from pprint import pprint
 from fibsem.segmentation.model import load_model
+import os
+from tqdm import tqdm
 
 from fibsem import config
 
 
 ### load evaluation folder 
+main_folder = r'C:\Users\rkan0039\Documents\detection_training\new_eval'
 
-eval_folder = r'C:\Users\rkan0039\Documents\detection_training\new_eval\label_01\{}'
+label_folder = "label_00"
+report_folder_name = f"report_{label_folder}"
 
-GT_data_file = r'C:\Users\rkan0039\Documents\detection_training\new_eval\label_01\data.csv'
+eval_folder = os.path.join(main_folder, label_folder)
+
+GT_data_file = os.path.join(eval_folder, "data.csv")
 
 GT_data = pd.read_csv(GT_data_file)
 
@@ -34,9 +40,13 @@ fnames = GT_data["label"]
 fnames_list = fnames.to_list()
 
 
-# b = GT_data.loc[GT_data["label"]==a]
-# print(b["p1.x"])
-filenames = sorted(glob.glob(eval_folder.format('*.tif')))
+
+report_folder_path = os.path.join(eval_folder, report_folder_name)
+
+if not os.path.exists(report_folder_path):
+    os.makedirs(report_folder_path)
+
+filenames = sorted(glob.glob(os.path.join(eval_folder,'*.tif')))
 checkpoint = r"C:\Users\rkan0039\Documents\detection_training\models\model4.pt"
 model = load_model(checkpoint,encoder="resnet34")
 
@@ -48,8 +58,12 @@ ML_p1_y = []
 ML_p2_x = []
 ML_p2_y = []
 
-accuracy_p1 = []
-accuracy_p2 = []
+p1_x_offset = []
+p1_y_offset = []
+p2_x_offset = []
+p2_y_offset = []
+p1_euc_dist = []
+p2_euc_dist = []
 
 labels_list = []
 p1_x_list =[]
@@ -69,11 +83,9 @@ detection_types = {
 }
 i=0
 
-for filename in filenames:
+for filename in tqdm(filenames,desc="Evaluating Images"):
 
     pic = filename.split('\\')[-1][:-4]
-    print(f"{i}/{len(filenames)}")
-    i+=1
 
     if pic in fnames_list:
 
@@ -106,7 +118,7 @@ for filename in filenames:
 
         det = locate_shift_between_features_v2(img, model, features=features, pixelsize=10e-9)
 
-        #plot_det_result_EVAL(det,save=True,save_path=save_path)
+        
         f1 = det.features[0]
         f2 = det.features[1]
 
@@ -121,7 +133,19 @@ for filename in filenames:
         ML_p2_x.append(convert_p2_x)
         ML_p2_y.append(convert_p2_y)
 
-        fig, ax = plt.subplots(1, 2, figsize=(12, 7))
+        p1_x_offset.append(p1_x - convert_p1_x)
+        p1_y_offset.append(p1_y - convert_p1_y)
+        p2_x_offset.append(p2_x - convert_p2_x)
+        p2_y_offset.append(p2_y - convert_p2_y)
+
+        p1_euc_dist.append(np.sqrt((p1_x - convert_p1_x)**2 + (p1_y - convert_p1_y)**2))
+        p2_euc_dist.append(np.sqrt((p2_x - convert_p2_x)**2 + (p2_y - convert_p2_y)**2))
+
+
+
+        
+
+        fig,ax = plt.subplots(1, 2, figsize=(12, 7))
 
         ax[0].imshow(det.image, cmap="gray")
         ax[1].imshow(det.mask)
@@ -184,20 +208,42 @@ for filename in filenames:
         ax[0].legend()
         ax[1].legend()
         plt.title(f"{label}")
-        plt.show()
+        
        
-        # plt.savefig(r'C:\Users\Rohit\Documents\UNI\DEMARCO\new_eval\label_00\mask.tiff')
+        report_fig_name = f"Report_{label}.png"
 
-        if i == 1:
-            break
+        fig.savefig(os.path.join(report_folder_path,report_fig_name))
+
+        plt.close(fig)
+
 
 
         
 
-data = {"label":labels_list,"p1.type":p1_type_list,"p1.x":p1_x_list,"p1.y":p1_y_list,"p2.type":p2_type_list,"p2.x":p2_x_list,"p2.y":p2_y_list,"ML_p1.x":ML_p1_x,"ML_p1.y":ML_p1_y,"ML_p2.x":ML_p2_x,"ML_p2.y":ML_p2_y}
+data = {
+    "label":labels_list,
+    "p1.type":p1_type_list,
+    "p1.x":p1_x_list,
+    "p1.y":p1_y_list,
+    "p2.type":p2_type_list,
+    "p2.x":p2_x_list,
+    "p2.y":p2_y_list,
+    "ML_p1.x":ML_p1_x,
+    "ML_p1.y":ML_p1_y,
+    "ML_p2.x":ML_p2_x,
+    "ML_p2.y":ML_p2_y,
+    "p1.x_offset":p1_x_offset,
+    "p1.y_offset":p1_y_offset,
+    "p2.x_offset":p2_x_offset,
+    "p2.y_offset":p2_y_offset,
+    "p1_euc_dist":p1_euc_dist,
+    "p2_euc_dist":p2_euc_dist,
+    }
 
 df = pd.DataFrame(data)
 
-# df.to_csv(r"C:\Users\rkan0039\Documents\detection_training\new_eval\label_00\ML_output.csv")
+report_name = f"Report_{label_folder}.csv"
+
+df.to_csv(os.path.join(eval_folder,report_name))
 
 
