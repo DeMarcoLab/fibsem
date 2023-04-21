960846fc1bcfea3cf022d5444806c19430199a4a
LucileNaegele <lucile.naegele@monash.edu>
2023-04-21 12:29:03 +1000

    added presets to milling settings

--- fibsem/structures.py
+++ fibsem/structures.py
@@ -893,6 +893,7 @@ class FibsemMillingSettings:
     hfw: float = 150e-6
     patterning_mode: str = "Serial" 
     application_file: str = "Si"
+    preset: str = "30 keV; UHR imaging"
 
     def __post_init__(self):
 
@@ -903,6 +904,7 @@ class FibsemMillingSettings:
         assert isinstance(self.hfw,(float,int)), f"invalid type for hfw, must be int or float, currently {type(self.hfw)}"
         assert isinstance(self.patterning_mode,str), f"invalid type for value for patterning_mode, must be str, currently {type(self.patterning_mode)}"
         assert isinstance(self.application_file,(str)), f"invalid type for value for application_file, must be str, currently {type(self.application_file)}"
+        assert isinstance(self.preset,(str)), f"invalid type for value for preset, must be str, currently {type(self.preset)}"
 
     def __to_dict__(self) -> dict:
 
@@ -914,6 +916,7 @@ class FibsemMillingSettings:
             "hfw": self.hfw,
             "patterning_mode": self.patterning_mode,
             "application_file": self.application_file,
+            "preset": self.preset,
         }
 
         return settings_dict
@@ -929,6 +932,7 @@ class FibsemMillingSettings:
             hfw=settings.get("hfw", 150e-6),
             patterning_mode=settings.get("patterning_mode", "Serial"),
             application_file=settings.get("application_file", "Si"),
+            preset=settings.get("preset", "30 keV; UHR imaging"),
         )
 
         return milling_settings
