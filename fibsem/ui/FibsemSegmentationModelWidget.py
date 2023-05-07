aabafae23bd812ad494b4b9731d896948bd70e6a
Patrick Cleeve <patrickcleeve2@gmail.com>
2023-05-07 20:30:08 +1000

    device

--- fibsem/ui/FibsemSegmentationModelWidget.py
+++ fibsem/ui/FibsemSegmentationModelWidget.py
@@ -100,7 +100,7 @@ def load_sam_model(encoder: str, checkpoint: str):
 
     sam = sam_model_registry[encoder](checkpoint=checkpoint)
 
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
     sam.to(device=device)
 
     model = SamPredictor(sam)
