f6e3f1ac0994d965ab84256e5b60f8fcd399b2d7
Patrick Cleeve <patrickcleeve2@gmail.com>
2023-05-07 20:19:56 +1000

    fibsem anything

--- fibsem/ui/FibsemLabellingUI.py
+++ fibsem/ui/FibsemLabellingUI.py
@@ -20,7 +20,24 @@ from fibsem.ui.FibsemSegmentationModelWidget import FibsemSegmentationModelWidge
 from fibsem.ui.FibsemModelTrainingWidget import FibsemModelTrainingWidget
 # BASE_PATH = os.path.join(os.path.dirname(fibsem.__file__), "config")
 
+from napari.layers import Points
+from typing import Any, Generator, Optional
 
+
+logging.basicConfig(level=logging.INFO)
+
+_DEBUG = True
+
+
+
+
+INSTRUCTIONS = {"data": "Select the folder containing the raw data\n",
+                "model": "Select the model to use for segmentation\n",
+                "ready": "Use the paintbrush to label the image.\n",
+                "model_loaded": "The Segmentation Model will assist with labelling images.\n",
+                "sam": "Click a point to segment the image.\nConfirm adds the SAM mask to the mask for the selected class.\nPress C to confirm, X to clear, D/F to increment the class\n"}
+
+# ref: https://github.com/JoOkuma/napari-segment-anything (Apache License 2.0)
 class FibsemLabellingUI(FibsemLabellingUI.Ui_Dialog, QtWidgets.QDialog):
     def __init__(
         self,
@@ -32,14 +49,23 @@ class FibsemLabellingUI(FibsemLabellingUI.Ui_Dialog, QtWidgets.QDialog):
         self.viewer = viewer
         self.idx = 0
         self.model = None
+        self.model_type = None
         self._model_assist = False
         self.DATA_LOADED = False
 
+        self.SAM_IMAGE_SET = False
+        self._sam_mask_layer = None
+        self._sam_pts_layer = None
 
         self.setup_connections()
 
+        if _DEBUG:
+            self.lineEdit_raw_data.setText("/Users/patrickcleeve/Documents/fibsem/fibsem/fibsem/log/data")
+            self.lineEdit_save_path.setText("/Users/patrickcleeve/Documents/fibsem/fibsem/fibsem/log/data")
+            self.spinBox_num_classes.setValue(3)
+
+
     def setup_connections(self):
-        print("setup connections")
 
         self.pushButton_load_data.clicked.connect(self.load_data)
         self.pushButton_next.clicked.connect(self.next_image)
@@ -54,22 +80,14 @@ class FibsemLabellingUI(FibsemLabellingUI.Ui_Dialog, QtWidgets.QDialog):
 
         self.checkBox_model_assist.stateChanged.connect(self.toggle_model_assist)
 
-    def toggle_model_assist(self):
-        self._model_assist = self.checkBox_model_assist.isChecked()
+        # SAM
+        self.pushButton_model_confirm.setVisible(False)
+        self.comboBox_model_class_index.setVisible(False)
+        self.label_model_class_index.setVisible(False)
+        self.pushButton_model_clear.setVisible(False)
 
-        if self.model is None:
-            self.label_model_info.setText(f"Please load a model.")
-        if self.DATA_LOADED:
-            self.update_image()
+        self._update_instructions()
 
-    def load_model(self):
-        self.model = self.model_widget.model
-        self.label_model_info.setText(
-            f"Model: {os.path.basename(self.model.checkpoint)}"
-        )
-
-        if self.DATA_LOADED:
-            self.update_image()
 
     def load_data(self):
 
@@ -111,7 +129,7 @@ class FibsemLabellingUI(FibsemLabellingUI.Ui_Dialog, QtWidgets.QDialog):
         bname = os.path.basename(self.fname).split(".")[0]
 
         # only resave the labels...
-        label = self._label_layer.data.astype(np.uint8)
+        label = self._mask_layer.data.astype(np.uint8)
 
         im = Image.fromarray(label)
         im.save(os.path.join(self.save_path, "labels", f"{bname}.tif"))  # or 'test.tif'
@@ -125,30 +143,66 @@ class FibsemLabellingUI(FibsemLabellingUI.Ui_Dialog, QtWidgets.QDialog):
         self.label_progress.setText(f"{self.idx + 1}/{len(self.filenames)}")
 
         # update / clear viewer
-        self.viewer.layers.clear()
         self.fname = self.filenames[self.idx]
         self.img = tff.imread(self.fname)
-        self._image_layer = self.viewer.add_image(
-            self.img,
-            name="Image",
-            opacity=0.7,
-            blending="additive",
-        )
+        self.SAM_IMAGE_SET = False
+
+        try:
+            self._image_layer.data = self.img
+        except:
+            self._image_layer = self.viewer.add_image(
+                self.img,
+                name="Image",
+                opacity=0.7,
+                blending="additive",
+            )
 
         label_image = self.get_label_image()
-        self._label_layer = self.viewer.add_labels(
-            label_image,
-            name="Mask",
-            opacity=0.7,
-            blending="additive",
-            color=CLASS_COLORS,
-        )
+
+        try: 
+            self._mask_layer.data = label_image
+        except:
+            self._mask_layer = self.viewer.add_labels(
+                label_image,
+                name="Mask",
+                opacity=0.7,
+                blending="additive",
+                color=CLASS_COLORS,
+            )
 
         # disable buttons
         IS_NOT_FIRST_INDEX = bool(self.idx != 0)
         IS_NOT_LAST_INDEX = bool(self.idx != int(len(self.filenames) - 1))
         self.pushButton_previous.setEnabled(IS_NOT_FIRST_INDEX)
         self.pushButton_next.setEnabled(IS_NOT_LAST_INDEX)
+
+
+        self._update_instructions()
+        self._set_sam_active()
+
+
+    def _update_instructions(self):
+
+
+        if not self.DATA_LOADED:
+            self.label_instructions.setText(INSTRUCTIONS["data"])
+            return
+
+        if self.model is None:
+            msg = INSTRUCTIONS["ready"] + INSTRUCTIONS["model"]
+            self.label_instructions.setText(msg)
+            return
+
+        if self.model is not None:
+            msg = INSTRUCTIONS["ready"] + INSTRUCTIONS["model_loaded"]
+            self.label_instructions.setText(msg)
+            
+
+        if self.model_type == "SegmentAnythingModel":
+            msg = INSTRUCTIONS["ready"] + INSTRUCTIONS["sam"]
+            self.label_instructions.setText(msg)
+             
+
 
     def get_label_image(self) -> np.ndarray:
 
@@ -161,12 +215,201 @@ class FibsemLabellingUI(FibsemLabellingUI.Ui_Dialog, QtWidgets.QDialog):
             label_image = tff.imread(label_fname)
             label_image = np.array(label_image, dtype=np.uint8)
 
+            msg = f"Loaded label image from {label_fname}"
+
         elif self._model_assist and self.model is not None:
-            label_image = self.model.inference(self.img, rgb=False)[0]
+
+            if self.model_type == "SegmentationModel":
+                label_image = self.model.inference(self.img, rgb=False)[0]
+            if self.model_type == "SegmentAnythingModel":
+                label_image = np.zeros_like(self.img)
+
+            msg = f"Generated label image using {self.model_type}"
+
         else:
             label_image = np.zeros_like(self.img)
 
+            msg = "No label image found, and no model loaded"
+        
+        napari.utils.notifications.show_info(msg)
+        
         return label_image
+
+    ####### MODEL
+    def toggle_model_assist(self):
+        self._model_assist = self.checkBox_model_assist.isChecked()
+
+        if self.model is None:
+            self.label_model_info.setText(f"Please load a model.")
+        if self.DATA_LOADED:
+            self.update_image()
+
+    def load_model(self):
+        self.model = self.model_widget.model
+        self.model_type = self.model_widget.model_type
+        self.label_model_info.setText(
+            f"Model: {os.path.basename(self.model.checkpoint)}"
+        )
+    
+        # specific layers for SAM model
+        if self.model_type == "SegmentAnythingModel":
+
+            self._add_sam_pts_layer()
+
+            self.comboBox_model_class_index.addItems([f"Class {i:02d}" for i in range(self.model.num_classes)])
+            self.pushButton_model_confirm.clicked.connect(self._confirm_model_class)
+            self.pushButton_model_clear.clicked.connect(self._clear_sam_data)
+            self.pushButton_model_confirm.setVisible(True)
+            self.comboBox_model_class_index.setVisible(True)
+            self.label_model_class_index.setVisible(True)
+            self.pushButton_model_clear.setVisible(True)
+
+            self.viewer.bind_key("C", self._confirm_model_class)
+            self.viewer.bind_key("X", self._clear_sam_data)
+            self.viewer.bind_key("D", self._decrement_sam_class)
+            self.viewer.bind_key("F", self._increment_sam_class)
+
+
+        napari.utils.notifications.show_info(
+            f"Loaded {self.model_type}: {os.path.basename(self.model.checkpoint)}"
+        )
+
+        if self.DATA_LOADED:
+            self.update_image()
+
+    ####### SAM
+    def _add_sam_pts_layer(self):
+        # add SAM points layer
+        self._sam_pts_layer = self.viewer.add_points(name="SAM Points")
+        self._sam_pts_layer.events.data.connect(self._update_sam_mask)
+        self._sam_pts_layer.mouse_drag_callbacks.append(self._mouse_button_modifier
+        )
+        self._sam_pts_layer.mode = "add"
+
+
+    def _mouse_button_modifier(self, _: Points, event) -> None:
+        self._sam_pts_layer.selected_data = []
+        if event.button == 1:
+            self._sam_pts_layer.current_face_color = "blue"
+        else:
+            self._sam_pts_layer.current_face_color = "red"
+
+        logging.info(f"Color: {self._sam_pts_layer.current_face_color}")
+
+    def _update_sam_mask(self, _: Optional[Any] = None) -> None:
+        points = self._sam_pts_layer.data
+        colors = self._sam_pts_layer.face_color
+
+        logging.info(f"POINTS: {len(points)}")
+
+        if len(points) > 0:
+            points = np.flip(points, axis=-1)
+            colors = self._sam_pts_layer.face_color
+            blue = [0, 0, 1, 1]
+            labels = np.all(colors == blue, axis=1)
+        else:
+            points = None
+            labels = None
+
+        # get the points from a click
+        # convert the current mask to a label image
+        # clear the current mask and point 
+
+        # this only needs to happen once when the image is loaded
+        # double chcek when the image is saved that it is grayscale
+        if not self.SAM_IMAGE_SET:
+            import cv2
+            logging.info("setting image embedding")
+            gray_img = cv2.cvtColor(self.img, cv2.COLOR_GRAY2RGB)
+            self.model.set_image(gray_img)
+            self.SAM_IMAGE_SET = True
+            self._logits = None
+
+        mask, _, self._logits = self.model.predict(
+            point_coords=points,
+            point_labels=labels,
+            box=None,
+            mask_input=self._logits,
+            multimask_output=False,
+        )
+
+        # add layer for mask
+        try:
+            self._sam_mask_layer.data = mask[0]
+        except:
+            self._sam_mask_layer = self.viewer.add_labels(
+                mask[0],
+                name="SAM Mask",
+                opacity=0.7,
+                blending="additive",
+                color={0:"black", 1:"cyan"},
+            )
+
+        self._set_sam_active()
+
+    def _set_sam_active(self):
+        try:
+            # set sam points layer active
+            self.viewer.layers.selection.active = self._sam_pts_layer
+            self._sam_pts_layer.mode = "add"  
+        except:
+            pass
+    
+    def _confirm_model_class(self, _: Optional[Any] = None)-> None:
+        
+        if self._sam_mask_layer is None:
+            return
+        
+        # take the current sam mask and add it to the Mask
+        sam_mask = self._sam_mask_layer.data
+
+        # get the current class index
+        class_index = self.comboBox_model_class_index.currentIndex()
+
+        # set all values of sam mask to class index
+        sam_mask[sam_mask > 0] = class_index
+
+        # add to, and update mask layer
+        mask = self._mask_layer.data
+        mask[sam_mask > 0] = sam_mask[sam_mask > 0]
+        self._mask_layer.data = mask
+
+        self._clear_sam_data()
+
+
+    def _increment_sam_class(self, _: Optional[Any] = None)-> None:
+
+        self._change_sam_class(1)
+
+    def _decrement_sam_class(self, _: Optional[Any] = None)-> None:
+
+        self._change_sam_class(-1)
+
+    def _change_sam_class(self,  val: int = 0) -> None:
+
+        # get the current class index
+        self._sam_class_index = self.comboBox_model_class_index.currentIndex()
+
+        self._sam_class_index += val
+
+        # clip to 0,number of classes
+        n_classes = self.comboBox_model_class_index.count()
+        self._sam_class_index = np.clip(self._sam_class_index, 0, n_classes-1)
+
+        self.comboBox_model_class_index.setCurrentIndex(self._sam_class_index)
+
+
+    def _clear_sam_data(self, _: Optional[Any] = None)-> None:
+
+        # clear points
+        # TODO: find the proper way to do this, not doing this way causses issues with face colour
+        self.viewer.layers.remove(self._sam_pts_layer)
+        self._add_sam_pts_layer()
+
+        # clear mask
+        if self._sam_mask_layer is not None:
+            self._sam_mask_layer.data = np.zeros_like(self._sam_mask_layer.data)
+
 
     def closeEvent(self, event):
         # try to save the current image on close
