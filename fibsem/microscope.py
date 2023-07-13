d86a97616d5520f11725e297b6adf4ef7b99e0b3
Patrick Cleeve <patrick.cleeve@monash.edu>
2023-07-13 16:46:12 +1000

    oops hehe

--- fibsem/microscope.py
+++ fibsem/microscope.py
@@ -993,7 +993,7 @@ class ThermoMicroscope(FibsemMicroscope):
         current_position = self.get_stage_position()
 
         # tilt flat for large rotations to prevent collisions
-        if rotation_angle_is_larger(stage_position.r, current_position.r):
+        if movement.rotation_angle_is_larger(stage_position.r, current_position.r):
 
             self.move_stage_absolute(FibsemStagePosition(t=0))
             logging.info(f"tilting to flat for large rotation.")
@@ -5185,56 +5185,3 @@ def _check_sputter(hardware_settings: FibsemHardware):
         raise NotImplementedError("The microscope does not have a GIS system.")
     if hardware_settings.gis_multichem == False:
         raise NotImplementedError("The microscope does not have a multichem system.")
-    
-
-
-# TODO: MOVE THIS SOMEWHERE ELSE
-
-
-def rotation_angle_is_larger(angle1: float, angle2: float, atol: float = 90) -> bool:
-    """Check the rotation angles are large
-
-    Args:
-        angle1 (float): angle1 (radians)
-        angle2 (float): angle2 (radians)
-        atol : tolerance (degrees)
-
-    Returns:
-        bool: rotation angle is larger than atol
-    """
-
-    return angle_difference(angle1, angle2) > (np.deg2rad(atol))
-
-
-def rotation_angle_is_smaller(angle1: float, angle2: float, atol: float = 5) -> bool:
-    """Check the rotation angles are large
-
-    Args:
-        angle1 (float): angle1 (radians)
-        angle2 (float): angle2 (radians)
-        atol : tolerance (degrees)
-
-    Returns:
-        bool: rotation angle is smaller than atol
-    """
-
-    return angle_difference(angle1, angle2) < (np.deg2rad(atol))
-
-
-def angle_difference(angle1: float, angle2: float) -> float:
-    """Return the difference between two angles, accounting for greater than 360, less than 0 angles
-
-    Args:
-        angle1 (float): angle1 (radians)
-        angle2 (float): angle2 (radians)
-
-    Returns:
-        float: _description_
-    """
-    angle1 %= 2 * np.pi
-    angle2 %= 2 * np.pi
-
-    large_angle = np.max([angle1, angle2])
-    small_angle = np.min([angle1, angle2])
-
-    return min((large_angle - small_angle), ((2 * np.pi + small_angle - large_angle)))
--- fibsem/movement.py
+++ fibsem/movement.py
@@ -154,3 +154,52 @@ if THERMO:
     #     return
 
 
+
+
+def rotation_angle_is_larger(angle1: float, angle2: float, atol: float = 90) -> bool:
+    """Check the rotation angles are large
+
+    Args:
+        angle1 (float): angle1 (radians)
+        angle2 (float): angle2 (radians)
+        atol : tolerance (degrees)
+
+    Returns:
+        bool: rotation angle is larger than atol
+    """
+
+    return angle_difference(angle1, angle2) > (np.deg2rad(atol))
+
+
+def rotation_angle_is_smaller(angle1: float, angle2: float, atol: float = 5) -> bool:
+    """Check the rotation angles are large
+
+    Args:
+        angle1 (float): angle1 (radians)
+        angle2 (float): angle2 (radians)
+        atol : tolerance (degrees)
+
+    Returns:
+        bool: rotation angle is smaller than atol
+    """
+
+    return angle_difference(angle1, angle2) < (np.deg2rad(atol))
+
+
+def angle_difference(angle1: float, angle2: float) -> float:
+    """Return the difference between two angles, accounting for greater than 360, less than 0 angles
+
+    Args:
+        angle1 (float): angle1 (radians)
+        angle2 (float): angle2 (radians)
+
+    Returns:
+        float: _description_
+    """
+    angle1 %= 2 * np.pi
+    angle2 %= 2 * np.pi
+
+    large_angle = np.max([angle1, angle2])
+    small_angle = np.min([angle1, angle2])
+
+    return min((large_angle - small_angle), ((2 * np.pi + small_angle - large_angle)))
