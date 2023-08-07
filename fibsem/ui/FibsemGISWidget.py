67316befd7401d01f3225f8b79994a147a5527f9
rohit.kannachel <rohit.kannachel@monash.edu>
2023-08-08 09:43:57 +1000

    cleaning up UI

--- fibsem/ui/FibsemGISWidget.py
+++ fibsem/ui/FibsemGISWidget.py
@@ -43,7 +43,10 @@ class FibsemGISWidget(FibsemGISWidget.Ui_Form, QtWidgets.QWidget):
         self.GIS_inserted = False
         self.GIS_insert_status_label.setText(f"GIS Status: inserted" if self.GIS_inserted else "GIS Status: retracted")
 
-        self.gis_lines = self.microscope.GIS_available_lines()
+        if isinstance(self.microscope, ThermoMicroscope):
+            self.gis_lines = self.microscope.multichem_available_lines()
+        else:
+            self.gis_lines = self.microscope.GIS_available_lines()
        
 
         self.gas_combobox.addItems(self.gis_lines)
@@ -54,7 +57,10 @@ class FibsemGISWidget(FibsemGISWidget.Ui_Form, QtWidgets.QWidget):
 
         self.gas_combobox.setCurrentText(self.gis_current_line)
 
-        self.gis_available_positions = self.microscope.GIS_available_positions()
+        if isinstance(self.microscope, ThermoMicroscope):
+            self.gis_available_positions = self.microscope.multichem_available_positions()
+        else:
+            self.gis_available_positions = self.microscope.GIS_available_positions()
         
         self.position_combobox.addItems(self.gis_available_positions)
 
@@ -103,37 +109,24 @@ class FibsemGISWidget(FibsemGISWidget.Ui_Form, QtWidgets.QWidget):
 
     def thermo_setup(self):
 
-        self.mc_lines = self.microscope.multichem_available_lines()
-        self.mc_current_line = self.mc_lines[0]
-        self.mc_available_positions = self.microscope.multichem_available_positions()
+        # self.mc_lines = self.microscope.multichem_available_lines()
+        # self.mc_current_line = self.mc_lines[0]
+        # self.mc_available_positions = self.microscope.multichem_available_positions()
         
 
-        self.GIS_inserted = True
-        self.insert_retract_gis()
-        self.GIS = True
-        self.GIS_radioButton.setChecked(True)
-        self.GIS = True
-        self.position_combobox.hide()
-        self.position_combobox.setEnabled(False)
+        # self.insert_retract_gis()
 
-        self.move_GIS_button.setEnabled(False)
-        self.move_GIS_button.hide()
+
+        # self.move_GIS_button.setEnabled(True)
+        # self.move_GIS_button.hide()
         
         self.gas_combobox.clear()
         self.gas_combobox.addItems(self.gis_lines)
         self.gas_combobox.setCurrentText(self.gis_current_line)
 
-        self.label_position.hide()
-        self.label_position.setEnabled(False)
-
-        self.insertGIS_button.setEnabled(True)
-        self.insertGIS_button.show()
 
         self.blankBeamcheckbox.setChecked(False)
 
-
-        self.current_position_label.setEnabled(False)
-        self.current_position_label.hide()
 
         app_files =  self.microscope.get_available_values(key="application_file")
 
@@ -149,62 +142,62 @@ class FibsemGISWidget(FibsemGISWidget.Ui_Form, QtWidgets.QWidget):
 
         self.update_ui()
 
-    def change_gis_multichem(self):
+    # def change_gis_multichem(self):
         
-        checked_button = self.sender()
+    #     checked_button = self.sender()
 
-        if not checked_button.isChecked():
-            return
+    #     if not checked_button.isChecked():
+    #         return
     
-        if checked_button == self.GIS_radioButton:
-            self.GIS = True
-            # self.position_combobox.setCurrentText(self.gis_current_line)
-            self.position_combobox.hide()
-            self.position_combobox.setEnabled(False)
+    #     if checked_button == self.GIS_radioButton:
+    #         self.GIS = True
+    #         # self.position_combobox.setCurrentText(self.gis_current_line)
+    #         self.position_combobox.hide()
+    #         self.position_combobox.setEnabled(False)
 
-            self.move_GIS_button.setEnabled(False)
-            self.move_GIS_button.hide()
+    #         self.move_GIS_button.setEnabled(False)
+    #         self.move_GIS_button.hide()
             
-            self.gas_combobox.clear()
-            self.gas_combobox.addItems(self.gis_lines)
-            self.gas_combobox.setCurrentText(self.gis_current_line)
+    #         self.gas_combobox.clear()
+    #         self.gas_combobox.addItems(self.gis_lines)
+    #         self.gas_combobox.setCurrentText(self.gis_current_line)
 
-            self.label_position.hide()
-            self.label_position.setEnabled(False)
+    #         self.label_position.hide()
+    #         self.label_position.setEnabled(False)
 
-            self.insertGIS_button.setEnabled(True)
-            self.insertGIS_button.show()
+    #         self.insertGIS_button.setEnabled(True)
+    #         self.insertGIS_button.show()
 
-            self.current_position_label.setEnabled(False)
-            self.current_position_label.hide()
+    #         self.current_position_label.setEnabled(False)
+    #         self.current_position_label.hide()
 
-            self.GIS_insert_status_label.setEnabled(True)
-            self.GIS_insert_status_label.show()
+    #         self.GIS_insert_status_label.setEnabled(True)
+    #         self.GIS_insert_status_label.show()
         
-        if checked_button == self.multichem_radioButton:
-            self.GIS = False
-            self.position_combobox.clear()
-            self.position_combobox.addItems(self.mc_available_positions)
-            self.position_combobox.setEnabled(True)
-            self.position_combobox.show()
+    #     if checked_button == self.multichem_radioButton:
+    #         self.GIS = False
+    #         self.position_combobox.clear()
+    #         self.position_combobox.addItems(self.mc_available_positions)
+    #         self.position_combobox.setEnabled(True)
+    #         self.position_combobox.show()
 
-            self.move_GIS_button.setEnabled(True)
-            self.move_GIS_button.show()
+    #         self.move_GIS_button.setEnabled(True)
+    #         self.move_GIS_button.show()
             
-            self.insertGIS_button.setEnabled(False)
-            self.insertGIS_button.hide()
+    #         self.insertGIS_button.setEnabled(False)
+    #         self.insertGIS_button.hide()
 
-            self.current_position_label.setEnabled(True)
-            self.current_position_label.show()
+    #         self.current_position_label.setEnabled(True)
+    #         self.current_position_label.show()
 
-            self.GIS_insert_status_label.setEnabled(False)
-            self.GIS_insert_status_label.hide()
+    #         self.GIS_insert_status_label.setEnabled(False)
+    #         self.GIS_insert_status_label.hide()
 
-            self.gas_combobox.clear()
-            self.gas_combobox.addItems(self.mc_lines)
-            self.gas_combobox.setCurrentText(self.mc_current_line)
+    #         self.gas_combobox.clear()
+    #         self.gas_combobox.addItems(self.mc_lines)
+    #         self.gas_combobox.setCurrentText(self.mc_current_line)
         
-        self.update_ui()
+    #     self.update_ui()
             
 
     def update_gas_protocol(self):
@@ -219,8 +212,8 @@ class FibsemGISWidget(FibsemGISWidget.Ui_Form, QtWidgets.QWidget):
         self.insertGIS_button.clicked.connect(self.insert_retract_gis)
         self.gas_combobox.currentIndexChanged.connect(self.update_ui)
         self.move_GIS_button.clicked.connect(self.move_gis)
-        self.GIS_radioButton.toggled.connect(self.change_gis_multichem)
-        self.multichem_radioButton.toggled.connect(self.change_gis_multichem)
+        # self.GIS_radioButton.toggled.connect(self.change_gis_multichem)
+        # self.multichem_radioButton.toggled.connect(self.change_gis_multichem)
         self.warm_button.clicked.connect(self.warm_up_gis)
         self.run_button.clicked.connect(self.run_gis)
         self.beamtype_combobox.setCurrentText("ION")
@@ -230,23 +223,15 @@ class FibsemGISWidget(FibsemGISWidget.Ui_Form, QtWidgets.QWidget):
 
     def warm_up_gis(self):
         
-        if self.GIS:
-            line_name = self.gis_current_line
-            self.microscope.GIS_heat_up(line_name)
-            self.temp_label.setText("Temp: Ready")
-        else:
-            line_name = self.mc_current_line
-            self.microscope.multichem_heat_up(line_name)
-            self.temp_label.setText("Temp: Ready")
+        line_name = self.gis_current_line
+        self.microscope.GIS_heat_up(line_name)
+        self.temp_label.setText("Temp: Ready")
+
 
     def move_gis(self):
 
-        if self.GIS:
-            position = self.position_combobox.currentText()
-            self.microscope.GIS_move_to(self.gis_current_line, position)
-        else:
-            position = self.position_combobox.currentText()
-            self.microscope.multichem_move_to(position)
+        position = self.position_combobox.currentText()
+        self.microscope.multichem_move_to(position)
         self.update_ui()
 
     def update_ui(self):
@@ -254,19 +239,20 @@ class FibsemGISWidget(FibsemGISWidget.Ui_Form, QtWidgets.QWidget):
         line_name = self.gas_combobox.currentText()
         if line_name == "":
             return
-        if self.GIS:
-            self.gis_current_line = line_name
-            temp_ready = "Ready" if self.microscope.GIS_temp_ready(self.gis_current_line) else "Need Warm Up"
-            self.temp_label.setText(f"Temp: {temp_ready}")
-        else:
-            self.mc_current_line = line_name
-            temp_ready = "Ready" if self.microscope.multichem_temp_ready(self.mc_current_line) else "Need Warm Up"
-            self.temp_label.setText(f"Temp: {temp_ready}")
-            
-        current_position_gis = self.microscope.GIS_position(self.gis_current_line)
-        current_position_multichem = self.microscope.multichem_position() if isinstance(self.microscope,(ThermoMicroscope,DemoMicroscope)) else None
 
-        current_position = current_position_gis if self.GIS else current_position_multichem
+        self.gis_current_line = line_name
+        # temp_ready = "Ready" if self.microscope.GIS_temp_ready(self.gis_current_line) else "Need Warm Up"
+        # self.temp_label.setText(f"Temp: {temp_ready}")
+       
+            
+        if isinstance(self.microscope, ThermoMicroscope):
+            current_position =self.microscope.multichem_position()
+            temp_ready = "Ready" if self.microscope.multichem_temp_ready(self.gis_current_line) else "Need Warm Up"
+        else:
+            temp_ready = "Ready" if self.microscope.GIS_temp_ready(self.gis_current_line) else "Need Warm Up"
+            current_position = self.microscope.GIS_position(self.gis_current_line)
+
+        self.temp_label.setText(f"Temp: {temp_ready}")
 
         self.position_combobox.setCurrentText(current_position)
         self.current_position_label.setText(f"Current Position: {current_position}")
@@ -286,18 +272,12 @@ class FibsemGISWidget(FibsemGISWidget.Ui_Form, QtWidgets.QWidget):
 
     def run_gis(self):
 
-        if self.GIS:
-            self.gas_protocol["gas"] = self.gis_current_line
-            self.microscope.setup_GIS(self.gas_protocol)
-            self.microscope.setup_GIS_pattern(self.gas_protocol)
-            self.microscope.run_GIS(self.gas_protocol)
-        else:
-            self.gas_protocol["gas"] = self.mc_current_line
-            self.microscope.run_Multichem(self.gas_protocol)
+        self.gas_protocol["gas"] = self.gis_current_line
+        self.microscope.run_Multichem(self.gas_protocol)
         
 
 
-def main():
+def main(): 
 
     viewer = napari.Viewer(ndisplay=2)
     movement_widget = FibsemGISWidget()
