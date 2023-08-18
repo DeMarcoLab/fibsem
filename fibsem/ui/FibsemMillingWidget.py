1d87162bf5d84ceb9fc3e3ef9c12acd008d4c6f1
Patrick Cleeve <patrick.cleeve@monash.edu>
2023-08-18 19:42:19 +1000

    add warning if pattern too small / hfw too big

--- fibsem/ui/FibsemMillingWidget.py
+++ fibsem/ui/FibsemMillingWidget.py
@@ -573,11 +573,12 @@ class FibsemMillingWidget(FibsemMillingWidget.Ui_Form, QtWidgets.QWidget):
             milling_stages = [milling_stages]
 
         # # check hfw threshold
-        # for stage in milling_stages:
-        #     if stage.pattern.name == "Trench":
-        #         if stage.pattern.protocol["trench_height"] / stage.pattern.protocol["hfw"] < cfg.MILL_HFW_THRESHOLD:
-        #             napari.utils.notifications.show_warning(f"Pattern dimensions are too small for milling. Please decrease the image hfw or increase the trench height.")
-        #             return
+        for stage in milling_stages:
+            if stage.pattern.name == "Trench":
+                if stage.pattern.protocol["trench_height"] / stage.milling.hfw < cfg.MILL_HFW_THRESHOLD:
+                    napari.utils.notifications.show_warning(f"Pattern dimensions are too small for milling. Please decrease the image hfw or increase the trench height.")
+                    _remove_all_layers(self.viewer)
+                    return
 
         t2 = time.time()
         try:
