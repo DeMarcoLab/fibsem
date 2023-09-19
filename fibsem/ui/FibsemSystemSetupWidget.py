bbcfd0831a13c1665584af09df036e5ac921edc0
Patrick Cleeve <patrick.cleeve@monash.edu>
2023-09-19 20:46:28 +1000

    tmp disable easter egg??

--- fibsem/ui/FibsemSystemSetupWidget.py
+++ fibsem/ui/FibsemSystemSetupWidget.py
@@ -6,7 +6,7 @@ import napari
 import napari.utils.notifications
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import pyqtSignal
-from playsound import playsound
+# from playsound import playsound
 
 import fibsem
 from fibsem import config as cfg
@@ -393,7 +393,7 @@ class FibsemSystemSetupWidget(FibsemSystemSetupWidget.Ui_Form, QtWidgets.QWidget
             self.connect(ip_address, manufacturer )
 
         self.update_ui()
-        playsound(r"C:\Users\Rohit\Documents\UNI\DEMARCO\code\fibsem\fibsem\ui\startup.mp3")
+        # playsound(r"C:\Users\Rohit\Documents\UNI\DEMARCO\code\fibsem\fibsem\ui\startup.mp3")
 
     def update_ui(self):
 
