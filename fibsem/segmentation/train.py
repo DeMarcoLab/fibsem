add fix for numeric scaling issues

fibsem/segmentation/train.py
@@ -72,7 +72,7 @@ def train(model, device, data_loader, criterion, optimizer, WANDB, ui):
         data_loader.set_description(f"Train Loss: {loss.item():.04f}")
 
 
-        if WANDB and i % 8 == 0: 
+        if WANDB and i % 16 == 0: 
             
             # TODO: this is really inefficient, re-running the model on the same image, just use existing outputs
             idx = random.choice(np.arange(0, images.shape[0]))
@@ -80,17 +80,7 @@ def train(model, device, data_loader, criterion, optimizer, WANDB, ui):
             output_mask = utils.decode_output(output)
 
             img_base = images[idx].detach().cpu().squeeze().numpy()
-            # output_mask = utils.decode_output(outputs[idx])
             gt_base = masks[idx].detach().cpu()[:, :, None].permute(2, 0, 1).numpy()
-
-            # stack = wandb.Image(
-            #     np.hstack(
-            #         [img_as_ubyte(gray2rgb(img_base)),  
-            #         utils.decode_segmap(gt_base), 
-            #         utils.decode_segmap(output_mask)
-            #         ]
-            #         ), caption="Train Image (Raw, GT, Pred)"
-            # )
             stack = _create_wandb_image(img_base, gt_base, output_mask, "Train Image (Raw, GT, Pred)")
             wandb.log({"train_loss": loss.item(), "train_image": stack})
 
@@ -108,7 +98,7 @@ def validate(model, device, data_loader, criterion, WANDB, ui):
 
     for i, (images, masks) in enumerate(val_loader):
 
-        # model.eval()
+        model.eval()
 
         # move img and mask to device, reshape mask
         images = images.to(device)
@@ -127,7 +117,7 @@ def validate(model, device, data_loader, criterion, WANDB, ui):
         val_loader.set_description(f"Val Loss: {loss.item():.04f}")
 
 
-        if WANDB and i % 8 == 0:
+        if WANDB and i % 16 == 0:
 
             output = model(images[0][None, :, :, :])
             output_mask = utils.decode_output(outputs)
@@ -234,7 +224,7 @@ def _setup_dataset(config:dict):
         label_paths= config["label_paths"], 
         num_classes=config["num_classes"], 
         batch_size=config["batch_size"],
-        val_split=config.get("split", 0.2),
+        val_split=config.get("split", 0.15),
         _validate_dataset=config.get("validate_dataset", True),
     )
 