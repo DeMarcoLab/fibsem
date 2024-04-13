b977fbece7a8bd43a4fb959356b6655e7d09d7e0
Patrick Cleeve <cleeve@delmic.com>
2024-04-13 22:35:56 +0200

    fix: improve formatting on milling progress bar

--- fibsem/ui/FibsemMillingWidget.py
+++ fibsem/ui/FibsemMillingWidget.py
@@ -787,10 +787,12 @@ class FibsemMillingWidget(FibsemMillingWidget.Ui_Form, QtWidgets.QWidget):
         idx = info['idx']
         total = info['total']
         est_time = info['est_time']
+        t_m = int(est_time // 60)
+        t_s = int(est_time % 60)
 
         self.progressBar_milling.setVisible(True)
         self.progressBar_milling.setValue(value*100)
-        self.progressBar_milling.setFormat(f"Milling Stage {idx+1}/{total}: {est_time:.1f}s remaining...")
+        self.progressBar_milling.setFormat(f"Milling Stage {idx+1}/{total}: {t_m:02d}:{t_s:02d} remaining...")
 
     def finish_progress_bar(self):
         self.progressBar_milling.setVisible(False)
