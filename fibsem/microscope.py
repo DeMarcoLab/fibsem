b80a038024bf6b84ae3296509bf0c2058d919022
Thomas M Fish <50103643+thomasmfish@users.noreply.github.com>
2025-05-20 15:04:01 +0100

    Add TrenchBitmapPattern and increase options for BitmapPattern

--- fibsem/microscope.py
+++ fibsem/microscope.py
@@ -2288,7 +2288,7 @@ class ThermoMicroscope(FibsemMicroscope):
         logging.debug({"msg": "draw_circle", "pattern_settings": pattern_settings.to_dict()})
         self._patterns.append(pattern)
         return pattern
-    
+
     @staticmethod
     def _bitmap_to_points(
         bitmap_image: NDArray[np.uint8],
@@ -2299,6 +2299,7 @@ class ThermoMicroscope(FibsemMicroscope):
         return points_array
 
     def draw_bitmap_pattern(self, pattern_settings: FibsemBitmapSettings):
+        # Get bitmap from pattern settings (from file or array)
         bitmap = pattern_settings.bitmap
         if isinstance(bitmap, np.ndarray):
             bitmap_pattern = BitmapPatternDefinition()
@@ -2328,6 +2329,38 @@ class ThermoMicroscope(FibsemMicroscope):
             depth=pattern_settings.depth,
             bitmap_pattern_definition=bitmap_pattern,
         )
+
+        if not np.isclose(pattern_settings.time, 0.0):
+            logging.debug(f"Setting pattern time to {pattern_settings.time}.")
+            pattern.time = pattern_settings.time
+
+        # set pattern rotation
+        pattern.rotation = pattern_settings.rotation
+
+        # set exclusion
+        pattern.is_exclusion_zone = pattern_settings.is_exclusion
+
+        # set scan direction
+        available_scan_directions = self.get_available_values("scan_direction")
+
+        if pattern_settings.scan_direction in available_scan_directions:
+            pattern.scan_direction = pattern_settings.scan_direction
+        else:
+            pattern.scan_direction = "TopToBottom"
+            logging.warning(f"Scan direction {pattern_settings.scan_direction} not supported. Using TopToBottom instead.")
+            logging.warning(f"Supported scan directions are: {available_scan_directions}")
+
+        # set passes
+        if pattern_settings.passes: # not zero
+            pattern.dwell_time = pattern.dwell_time * (pattern.pass_count / pattern_settings.passes)
+
+            # NB: passes, time, dwell time are all interlinked, therefore can only adjust passes indirectly
+            # if we adjust passes directly, it just reduces the total time to compensate, rather than increasing the dwell_time
+            # NB: the current must be set before doing this, otherwise it will be out of range
+
+        # restore default application file
+        self.connection.patterning.set_default_application_file(self._default_application_file)
+
 
         logging.debug({"msg": "draw_bitmap_pattern", "pattern_settings": pattern_settings.to_dict()})
         self._patterns.append(pattern)
